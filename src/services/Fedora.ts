c3ea496945a5df6162a6928e9ade9fe79822a6ed
Christopher Hallberg <challber@villanova.edu>
2021-02-19 15:06:19 -0500

    Fedora DC massaged into id-title JSON.

--- src/services/Fedora.ts
+++ src/services/Fedora.ts
@@ -8,21 +8,17 @@ class Fedora {
     }
 
     async getDC(pid) {
-        console.log("Fedora::getDC");
         let options = {
             username: "fedoraAdmin", // Basic Auth
             password: "fedoraAdmin"
         };
         try {
             let url = this.baseUrl + "/" + pid + "/DC";
-            console.log(url, options);
             var res = await http("get", url, null, options);
-            // TODO: Error
-            // TODO: Massage????
             console.log("Fedora::then");
             return res.body;
         } catch (e) {
-            console.log("that failed", e);
+            console.log("edora::getDC failed", e);
         }
     }
 }
--- src/services/SolrIndexer.ts
+++ src/services/SolrIndexer.ts
@@ -5,15 +5,28 @@ class SolrIndexer {
 
     constructor() {
         // TODO: Config
-        // TODO: Make Fedora connection
+        // Make Fedora connection
         this.fedora = new Fedora();
     }
 
     async getFields(pid: string) {
-        // TODO: Use Fedora to get data
-        let dc = this.fedora.getDC(pid);
-        console.log("SI:then");
-        return dc;
+        // Use Fedora to get data
+        // TODO: type
+        let dc = await this.fedora.getDC(pid);
+
+        // Massage data
+        let fields: any = {};
+        for (let field of dc.children) {
+            // TODO: Abstract to key-value map
+            if (field.name == "dc:title") {
+                fields.title = field.value;
+            }
+            if (field.name == "dc:identifier") {
+                fields.id = field.value;
+            }
+        }
+
+        return fields;
     }
 }
 
