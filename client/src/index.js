75b6269d1912e7adff396ccd1eb10772c56693d7
Demian Katz <demian.katz@villanova.edu>
2021-03-16 13:06:31 -0400

    CSS cleanup.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './css/application.css';
 import VuDLPrep from './VuDLPrep';
 import * as serviceWorker from './serviceWorker';
 
