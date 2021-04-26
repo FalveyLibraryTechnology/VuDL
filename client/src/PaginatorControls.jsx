12103aa3768fbdaffcbe1606e8b71a635e7e4798
Chris Hallberg <crhallberg@gmail.com>
2021-04-26 18:09:03 -0400

    Revert manually fixed file to reduce auto diffs

--- client/src/PaginatorControls.jsx
+++ client/src/PaginatorControls.jsx
@@ -1,48 +1,54 @@
-var React = require("react");
-var MagicLabeler = require("./MagicLabeler");
-var PaginatorControlGroup = require("./PaginatorControlGroup");
-var ZoomToggleButton = require("./ZoomToggleButton");
+var React = require('react');
+var MagicLabeler = require('./MagicLabeler');
+var PaginatorControlGroup = require('./PaginatorControlGroup');
+var ZoomToggleButton = require('./ZoomToggleButton');
 
-class PaginatorControls extends React.Component {
-    approveCurrentPageLabel() {
+class PaginatorControls extends React.Component{
+    approveCurrentPageLabel = () => {
         this.setLabel(this.getLabel(true));
     }
 
-    getLabel(useMagic) {
-        if (typeof useMagic === "undefined") {
+    getLabel = (useMagic) => {
+        if (typeof useMagic === 'undefined') {
             useMagic = true;
         }
         var label = this.labelInput.value;
-        return label.length === 0 && useMagic
+        return (label.length === 0 && useMagic)
             ? this.props.paginator.getLabel(this.props.paginator.state.currentPage)
             : label;
     }
 
-    setLabel(label) {
+    setLabel = (label) => {
         this.props.paginator.setLabel(this.props.paginator.state.currentPage, label);
     }
 
-    setLabelPrefix(str) {
-        this.setLabel(MagicLabeler.replaceLabelPart(this.getLabel(), "prefix", str, true));
+    setLabelPrefix = (str) => {
+        this.setLabel(
+            MagicLabeler.replaceLabelPart(this.getLabel(), 'prefix', str, true)
+        );
     }
 
-    setLabelBody(str) {
-        this.setLabel(MagicLabeler.replaceLabelPart(this.getLabel(), "label", str));
+    setLabelBody = (str) => {
+        this.setLabel(
+            MagicLabeler.replaceLabelPart(this.getLabel(), 'label', str)
+        );
     }
 
-    setLabelSuffix(str) {
-        this.setLabel(MagicLabeler.replaceLabelPart(this.getLabel(), "suffix", str, true));
+    setLabelSuffix = (str) => {
+        this.setLabel(
+            MagicLabeler.replaceLabelPart(this.getLabel(), 'suffix', str, true)
+        );
     }
 
-    toggleBrackets() {
+    toggleBrackets = () => {
         this.setLabel(MagicLabeler.toggleBrackets(this.getLabel()));
     }
 
-    toggleCase() {
+    toggleCase = () => {
         this.setLabel(MagicLabeler.toggleCase(this.getLabel()));
     }
 
-    toggleRoman() {
+    toggleRoman = () => {
         var label = MagicLabeler.toggleRoman(this.getLabel());
         if (label === false) {
             return alert("Roman numeral toggle not supported for this label.");
@@ -50,84 +56,37 @@ class PaginatorControls extends React.Component {
         this.setLabel(label);
     }
 
-    updateCurrentPageLabel() {
+    updateCurrentPageLabel = () => {
         this.setLabel(this.getLabel(false));
     }
 
-    render() {
+    render = () => {
         return (
             <div className="controls">
                 <div className="group">
                     <div className="status"></div>
-                    <input
-                        type="text"
-                        value={this.props.paginator.getLabel(this.props.paginator.state.currentPage, false)}
-                        ref={(l) => {
-                            this.labelInput = l;
-                        }}
-                        id="page"
-                        onChange={this.updateCurrentPageLabel}
-                    />
+                    <input type="text" value={this.props.paginator.getLabel(this.props.paginator.state.currentPage, false)} ref={(l) => { this.labelInput = l; }} id="page" onChange={this.updateCurrentPageLabel} />
                     <button onClick={this.props.paginator.prevPage}>Prev</button>
-                    <button
-                        onClick={function () {
-                            this.approveCurrentPageLabel();
-                            this.props.paginator.nextPage();
-                        }.bind(this)}
-                    >
-                        Next
-                    </button>
+                    <button onClick={function() { this.approveCurrentPageLabel(); this.props.paginator.nextPage(); }.bind(this)}>Next</button>
                 </div>
                 <div className="top">
                     <ZoomToggleButton paginator={this.props.paginator} />
-                    <button
-                        className="primary"
-                        onClick={function () {
-                            this.approveCurrentPageLabel();
-                            this.props.paginator.save(false);
-                        }.bind(this)}
-                    >
-                        Save
-                    </button>
-                    <button
-                        className="primary"
-                        onClick={function () {
-                            this.approveCurrentPageLabel();
-                            this.props.paginator.save(true);
-                        }.bind(this)}
-                    >
-                        Save and Publish
-                    </button>
+                    <button className="primary" onClick={function() { this.approveCurrentPageLabel(); this.props.paginator.save(false); }.bind(this)}>Save</button>
+                    <button className="primary" onClick={function() { this.approveCurrentPageLabel(); this.props.paginator.save(true); }.bind(this)}>Save and Publish</button>
                 </div>
-                <PaginatorControlGroup callback={this.setLabelPrefix} label="prefixes">
-                    {MagicLabeler.prefixes}
-                </PaginatorControlGroup>
-                <PaginatorControlGroup callback={this.setLabelBody} label="labels">
-                    {MagicLabeler.labels}
-                </PaginatorControlGroup>
-                <PaginatorControlGroup callback={this.setLabelSuffix} label="suffixes">
-                    {MagicLabeler.suffixes}
-                </PaginatorControlGroup>
+                <PaginatorControlGroup callback={this.setLabelPrefix} label="prefixes">{MagicLabeler.prefixes}</PaginatorControlGroup>
+                <PaginatorControlGroup callback={this.setLabelBody}   label="labels"  >{MagicLabeler.labels}</PaginatorControlGroup>
+                <PaginatorControlGroup callback={this.setLabelSuffix} label="suffixes">{MagicLabeler.suffixes}</PaginatorControlGroup>
                 <div className="toggles group">
-                    <button onClick={this.toggleBrackets} title="Toggle Brackets">
-                        [ ]
-                    </button>
-                    <button onClick={this.toggleCase} title="Toggle Case">
-                        <i className="fa fa-text-height"></i>
-                    </button>
-                    <button onClick={this.toggleRoman} title="Toggle Roman Numerals">
-                        4<i className="fa fa-fw fa-arrows-alt-h"></i>IV
-                    </button>
+                    <button onClick={this.toggleBrackets} title="Toggle Brackets">[ ]</button>
+                    <button onClick={this.toggleCase} title="Toggle Case"><i className="fa fa-text-height"></i></button>
+                    <button onClick={this.toggleRoman} title="Toggle Roman Numerals">4<i className="fa fa-fw fa-arrows-alt-h"></i>IV</button>
                 </div>
-                <button onClick={this.props.paginator.autonumberFollowingPages} title="Autonumber Following Pages">
-                    <i className="fa fa-sort-numeric-down"></i>
-                </button>
-                <button className="danger" onClick={this.props.paginator.deletePage} title="Delete Current Page">
-                    <i className="fa fa-fw fa-trash"></i> Delete Current Page
-                </button>
+                <button onClick={this.props.paginator.autonumberFollowingPages} title="Autonumber Following Pages"><i className="fa fa-sort-numeric-down"></i></button>
+                <button className="danger" onClick={this.props.paginator.deletePage} title="Delete Current Page"><i className="fa fa-fw fa-trash"></i> Delete Current Page</button>
             </div>
         );
     }
-}
+};
 
 module.exports = PaginatorControls;
