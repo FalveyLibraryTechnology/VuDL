31bf324303d5e0a815e3c5787cb4b19eaf238556
Chris Hallberg <crhallberg@gmail.com>
2021-05-06 11:01:27 -0400

    Testing basic lint problem!

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,7 +1,8 @@
 import express = require("express");
 const router = express.Router();
 
-router.use(function timeLog(req, res, next) {
+router.use
+(function timeLog(req, res, next) {
     console.log("Time: ", Date.now());
     next();
 });
