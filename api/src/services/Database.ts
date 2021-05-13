4cee3cd69cc6c8759c4ab3a4f702b0257547650a
Chris Hallberg <crhallberg@gmail.com>
2021-05-13 14:26:35 -0400

    Make db instance and async. Move to data folder.

--- api/src/services/Database.ts
+++ api/src/services/Database.ts
@@ -8,35 +8,47 @@ interface User {
     hash: string;
 }
 
-const dbFilename = "./auth.db";
-const config: Knex.Config = {
-    client: "sqlite3",
-    connection: {
-        filename: dbFilename,
-    },
-};
+let db: Knex = null;
+async function getDatabase(): Promise<Knex> {
+    if (db === null) {
+        console.log("Database:setup");
+        const dbFilename = "./data/auth.sqlite3";
+        const config: Knex.Config = {
+            client: "sqlite3",
+            connection: {
+                filename: dbFilename,
+            },
+            useNullAsDefault: true,
+        };
 
-const db = knex(config);
+        db = knex(config);
 
-if (!fs.existsSync(dbFilename)) {
-    db.schema.createTable("users", table => {
-        table.increments("id");
-        table.string("username");
-        table.string("password");
-        table.string("hash");
-    }).then(() => {
-        const users = [
-            { username: "chris", password: "air", hash: "V1StGXR8_Z5jdHi6B-myT" },
-            { username: "geoff", password: "earth", hash: "CuhFfwkebs3RKr1Zo_Do_" },
-            { username: "dkatz", password: "avatar", hash: "_HPZZ6uCouEU5jy-AYrDd" },
-        ];
-        for (const user of users) {
-            db("users").insert(user);
+        if (!fs.existsSync(dbFilename)) {
+            console.log("Database:createTable");
+            await db.schema.createTable("users", (table) => {
+                table.increments("id");
+                table.string("username");
+                table.string("password");
+                table.string("hash");
+            });
+            console.log("Database:insert");
+            const users = [
+                { username: "chris", password: "air", hash: "V1StGXR8_Z5jdHi6B-myT" },
+                { username: "geoff", password: "earth", hash: "CuhFfwkebs3RKr1Zo_Do_" },
+                { username: "dkatz", password: "avatar", hash: "_HPZZ6uCouEU5jy-AYrDd" },
+            ];
+            for (const user of users) {
+                await db("users").insert(user);
+            }
         }
-    });
+    }
+
+    console.log("Database:ready");
+    return db;
 }
 
 export async function getUserBy(key, val): Promise<User> {
-    let users = await db<User>('users').where(key, val);
+    let db = await getDatabase();
+    let users = await db<User>("users").where(key, val);
     return users[0] ?? null;
 }
