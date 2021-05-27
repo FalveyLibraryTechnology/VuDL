2dfe61a082ee22324e3e66249c4e07ff3bd8e849
Chris Hallberg <crhallberg@gmail.com>
2021-05-27 13:43:54 -0400

    Fix database creation race issue.

--- api/src/services/Database.ts
+++ api/src/services/Database.ts
@@ -18,11 +18,38 @@ interface Token {
     user_id: number;
 }
 
+async function createDatabase(db): Promise<void> {
+    console.log("Database:createTables");
+    await db.schema.dropTableIfExists("users");
+    await db.schema.createTable("users", (table) => {
+        table.increments("id");
+        table.string("username");
+        table.string("password");
+        table.string("hash");
+    });
+    await db.schema.dropTableIfExists("tokens");
+    await db.schema.createTable("tokens", (table) => {
+        table.string("token").primary();
+        table.timestamp("created_at").defaultTo(Date.now());
+        table.integer("user_id").unsigned().references("users.id");
+    });
+    const users = [
+        { username: "geoff", password: "earth", hash: "CuhFfwkebs3RKr1Zo_Do_" },
+        { username: "chris", password: "air", hash: "V1StGXR8_Z5jdHi6B-myT" },
+        { username: "dkatz", password: "avatar", hash: "_HPZZ6uCouEU5jy-AYrDd" },
+    ];
+    for (const user of users) {
+        console.log(`Database:insert: ${user.username}`);
+        await db("users").insert(user);
+    }
+}
+
 let db: Knex = null;
-const REBUILD_DB = true;
+const REBUILD_DB = false;
 async function getDatabase(): Promise<Knex> {
     if (db === null) {
-        console.log("Database:setup");
+        console.log("Database:connect");
+
         const config: Knex.Config = {
             client: "sqlite3",
             connection: {
@@ -31,33 +58,10 @@ async function getDatabase(): Promise<Knex> {
             useNullAsDefault: true,
         };
 
-        db = knex(config);
+        db = await knex(config);
 
         if (REBUILD_DB || !fs.existsSync(dbFilename)) {
-            console.log("Database:createTables");
-            await db.schema
-                .dropTableIfExists("users")
-                .createTable("users", (table) => {
-                    table.increments("id");
-                    table.string("username");
-                    table.string("password");
-                    table.string("hash");
-                })
-                .dropTableIfExists("tokens")
-                .createTable("tokens", (table) => {
-                    table.string("token").primary();
-                    table.timestamp("created_at").defaultTo(Date.now());
-                    table.integer("user_id").unsigned().references("users.id");
-                });
-            console.log("Database:insert");
-            const users = [
-                { username: "geoff", password: "earth", hash: "CuhFfwkebs3RKr1Zo_Do_" },
-                { username: "chris", password: "air", hash: "V1StGXR8_Z5jdHi6B-myT" },
-                { username: "dkatz", password: "avatar", hash: "_HPZZ6uCouEU5jy-AYrDd" },
-            ];
-            for (const user of users) {
-                await db("users").insert(user);
-            }
+            await createDatabase(db);
         }
 
         console.log("Database:ready");
