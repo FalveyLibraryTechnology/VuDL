019b4f376e0c5b522cf3b6bb3b16c22d66187ba3
Demian Katz <demian.katz@villanova.edu>
2021-07-01 14:37:09 -0400

    Simplify code.

--- api/src/routes/api.ts
+++ api/src/routes/api.ts
@@ -84,11 +84,10 @@ router.put("/:category/:job", sanitizeParameters(), requireToken, function (req,
     const job = getJobFromRequest(req);
     const raw = req.body;
     try {
-        if (!job == null) {
-            job.metadata.raw = raw;
-        } else {
+        if (job == null) {
             return res.status(404).json({ error: "Job not found" });
         }
+        job.metadata.raw = raw;
         const problems = job.metadata.status.file_problems as Record<string, Array<string>>;
         if (problems.added.length > 0 || problems.deleted.length > 0) {
             throw new Error("file problem found");
@@ -109,12 +108,11 @@ router.get("/:category/:job/:image/:size", sanitizeParameters(), requireToken, a
     const image: string = req.params.image;
     const size: string = req.params.size;
     const job = getJobFromRequest(req);
-    if (job !== null) {
-        const deriv = await job.getImage(image).derivative(legalSizes[size] ?? "THUMBNAIL");
-        res.sendFile(deriv);
-    } else {
-        res.status(404).json({ error: "Job not found" });
+    if (job == null) {
+        return res.status(404).json({ error: "Job not found" });
     }
+    const deriv = await job.getImage(image).derivative(legalSizes[size] ?? "THUMBNAIL");
+    res.sendFile(deriv);
 });
 
 router.delete(
@@ -128,14 +126,13 @@ router.delete(
             const job = getJobFromRequest(req);
             if (job == null) {
                 return res.status(404).json({ error: "Job not found" });
+            }
+            const imageObj = job.getImage(image);
+            if (imageObj !== null) {
+                imageObj.delete();
+                res.json({ status: "ok" });
             } else {
-                const imageObj = job.getImage(image);
-                if (imageObj !== null) {
-                    imageObj.delete();
-                    res.json({ status: "ok" });
-                } else {
-                    res.status(404).json({ status: "image missing" });
-                }
+                res.status(404).json({ status: "image missing" });
             }
         }
     )
