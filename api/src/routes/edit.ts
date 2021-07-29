852a600c01d3eead1d77eb7afae594c4e1854afd
Demian Katz <demian.katz@villanova.edu>
2021-07-29 13:55:48 -0400

    Adjust route path.

--- api/src/routes/edit.ts
+++ api/src/routes/edit.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 import { setupPassport, requireToken } from "./auth";
 import { sanitizeParameters } from "./sanitize";
 
-router.get("/", requireToken, function (req, res) {
+router.get("/models", requireToken, function (req, res) {
     res.json({ CollectionModels: Config.getInstance().collectionModels, DataModels: Config.getInstance().dataModels });
 });
 
