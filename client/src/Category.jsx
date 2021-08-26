f78c1d0e97f43c052547954105fc49caa45b1ede
Adrian C <cagaananadrian@gmail.com>
2021-08-26 18:57:22 -0400

    Add unit testing and function component template

--- client/src/Category.jsx
+++ client/src/Category.jsx
@@ -1,58 +1,32 @@
 import React from "react";
+import { useSessionStorage } from "./sessionStorage";
 import PropTypes from "prop-types";
-
 import JobList from "./JobList";
 
-class Category extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { open: this.checkStorage() };
-        this.toggle = this.toggle.bind(this);
-    }
-
-    checkStorage() {
-        return typeof window.sessionStorage !== "undefined"
-            ? "true" === window.sessionStorage.getItem("open-" + this.props.data.category)
-            : false;
-    }
-
-    setStorage(isOpen) {
-        if (typeof window.sessionStorage !== "undefined") {
-            window.sessionStorage.setItem("open-" + this.props.data.category, isOpen ? "true" : "false");
-        }
-    }
-
-    toggle(e) {
+const Category = ({ data }) => {
+    const { jobs, category } = data;
+    const [open, setOpen] = useSessionStorage("open-" + category, false);
+    const toggle = (e) => {
         e.preventDefault();
         e.stopPropagation();
-        this.setStorage(!this.state.open);
-        this.setState({ open: !this.state.open });
-    }
-
-    render() {
-        var header = this.props.data.jobs.length ? (
-            <h2>
-                <button className="btn-link" onClick={this.toggle}>
-                    {this.state.open ? "[–]" : "[+]"}
-                </button>{" "}
-                {this.props.data.category}
-            </h2>
-        ) : (
-            <h2>{this.props.data.category + " [no jobs]"}</h2>
-        );
-        var joblist = this.state.open ? (
-            <JobList category={this.props.data.category} data={this.props.data.jobs} />
-        ) : (
-            ""
-        );
-        return (
-            <div className="jobCategory">
-                {header}
-                {joblist}
-            </div>
-        );
-    }
-}
+        setOpen(!open);
+    };
+    return (
+        <div className="jobCategory">
+            {jobs.length ? (
+                <h2>
+                    <button className="btn-link" onClick={toggle}>
+                        {open ? "[–]" : "[+]"}
+                    </button>{" "}
+                    {category}
+                </h2>
+            ) : (
+                <h2>{category + " [no jobs]"}</h2>
+            )}
+            {open && <JobList category={category} data={jobs} />}
+        </div>
+    );
+};
 
 Category.propTypes = {
     data: PropTypes.exact({
