0b8099574121759b68c0b937998347c1a057935d
Demian Katz <demian.katz@villanova.edu>
2021-09-22 16:17:06 -0400

    Modernize code.

--- client/src/Editor/CreateObject.jsx
+++ client/src/Editor/CreateObject.jsx
@@ -1,92 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TreeView, TreeItem } from "@material-ui/lab";
 
 import AjaxHelper from "../AjaxHelper";
 
-export default class CreateObject extends React.Component {
-    constructor(props) {
-        super(props);
-        this.ajax = AjaxHelper.getInstance();
-        this.state = { models: {}, selectedModel: null };
-        // BIND
-        this.handleSelect = this.handleSelect.bind(this);
-    }
+const CreateObject = () => {
+    const ajax = AjaxHelper.getInstance();
+    const [models, setModels] = useState({});
+    const [selectedModel, setSelectedModel] = useState("");
 
-    componentDidMount() {
-        this.ajax.getJSONPromise(this.ajax.apiUrl + "/edit/models").then((json) => {
-            this.setState({ models: json });
+    useEffect(() => {
+        ajax.getJSONPromise(ajax.apiUrl + "/edit/models").then((json) => {
+            setModels(json);
         });
-    }
+    }, []);
 
-    renderTree(nodes) {
-        return (
-            <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
-                {Array.isArray(nodes.children) ? nodes.children.map((node) => this.renderTree(node)) : null}
-            </TreeItem>
-        );
-    }
-
-    handleSelect(event, model) {
+    function handleSelect(event, model) {
         event.preventDefault();
         // Ignore categories
         if (model.slice(0, 2) == "__") {
             return;
         }
-        this.setState({ selectedModel: model });
+        setSelectedModel(model);
         return false;
     }
 
-    render() {
-        let categories = [];
-        for (let category in this.state.models) {
-            let children = [];
-            for (let model in this.state.models[category]) {
-                let value = this.state.models[category][model];
-                children.push(<TreeItem key={model} nodeId={value} label={model} />);
-            }
-            categories.push(
-                <TreeItem key={category} nodeId={`__${category}`} label={category}>
-                    {children}
-                </TreeItem>
-            );
+    let categories = [];
+    for (let category in models) {
+        let children = [];
+        for (let model in models[category]) {
+            let value = models[category][model];
+            children.push(<TreeItem key={model} nodeId={value} label={model} />);
         }
-        return (
-            <form className="editor__create-object" method="post" action={this.ajax.apiUrl + "/edit/object/new"}>
-                <label>
-                    Title
-                    <input type="text" name="title" required />
-                </label>
-
-                <label>
-                    Parent ID
-                    <input type="text" name="parent" required />
-                </label>
-
-                <label>
-                    <input type="radio" name="state" value="Active" />
-                    Active
-                </label>
-
-                <label>
-                    <input type="radio" name="state" value="Inactive" defaultChecked />
-                    Inactive
-                </label>
-
-                <label>
-                    <input type="radio" name="state" value="Deleted" />
-                    Deleted
-                </label>
-
-                <label>
-                    Select Model Type:
-                    <b>{this.state.selectedModel}</b>
-                </label>
-                <TreeView defaultCollapseIcon={"➖"} defaultExpandIcon={"➕"} onNodeSelect={this.handleSelect}>
-                    {categories}
-                </TreeView>
-
-                {this.state.selectedModel && <button>Create Object</button>}
-            </form>
+        categories.push(
+            <TreeItem key={category} nodeId={`__${category}`} label={category}>
+                {children}
+            </TreeItem>
         );
     }
-}
+    return (
+        <form className="editor__create-object" method="post" action={ajax.apiUrl + "/edit/object/new"}>
+            <label>
+                Title
+                <input type="text" name="title" required />
+            </label>
+
+            <label>
+                Parent ID
+                <input type="text" name="parent" required />
+            </label>
+
+            <label>
+                <input type="radio" name="state" value="Active" />
+                Active
+            </label>
+
+            <label>
+                <input type="radio" name="state" value="Inactive" defaultChecked />
+                Inactive
+            </label>
+
+            <label>
+                <input type="radio" name="state" value="Deleted" />
+                Deleted
+            </label>
+
+            <label>
+                Select Model Type:
+                <b>{selectedModel}</b>
+            </label>
+            <TreeView defaultCollapseIcon={"➖"} defaultExpandIcon={"➕"} onNodeSelect={handleSelect}>
+                {categories}
+            </TreeView>
+
+            {selectedModel && <button>Create Object</button>}
+        </form>
+    );
+};
+
+export default CreateObject;
