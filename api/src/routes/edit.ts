9d82a3f31482435b6731c8180ff1e7a349bd3c19
Demian Katz <demian.katz@villanova.edu>
2021-09-23 07:49:53 -0400

    Pass values to API successfully.

--- api/src/routes/edit.ts
+++ api/src/routes/edit.ts
@@ -1,4 +1,5 @@
 import express = require("express");
+import bodyParser = require("body-parser");
 import Config from "../models/Config";
 const router = express.Router();
 import { requireToken } from "./auth";
@@ -7,9 +8,9 @@ router.get("/models", requireToken, function (req, res) {
     res.json({ CollectionModels: Config.getInstance().collectionModels, DataModels: Config.getInstance().dataModels });
 });
 
-router.post("/object/new", requireToken, function (req, res) {
-    console.log("form post! object new!", req.params);
-    res.json({ TODO: "Handle this form", ...req.params });
+router.post("/object/new", requireToken, bodyParser.json(), function (req, res) {
+    console.log("form post! object new!", req.body);
+    res.json({ TODO: "Handle this form", ...req.body });
 });
 
 module.exports = router;
