61f804e01a1502234a0f4a5a67447403af92336d
Demian Katz <demian.katz@villanova.edu>
2021-09-30 15:27:46 -0400

    Add submit test.

--- client/src/Editor/CreateObject.test.js
+++ client/src/Editor/CreateObject.test.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { beforeEach, describe, expect, it } from "@jest/globals";
-import { shallow } from "enzyme";
+import { beforeEach, describe, expect, it, jest } from "@jest/globals";
+import { act } from "react-dom/test-utils";
+import { mount, shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import CreateObject from "./CreateObject";
+import AjaxHelper from "../AjaxHelper";
+
+jest.mock("../AjaxHelper");
+
+let nodeSelectFunction = null;
+jest.mock("@material-ui/lab", function () {
+    return {
+        TreeView: ({ onNodeSelect }) => {
+            nodeSelectFunction = onNodeSelect;
+            return "TreeView";
+        },
+        TreeItem: () => "TreeItem",
+    };
+});
 
 describe("CreateObject", () => {
+    let ajax;
     let props;
 
     beforeEach(() => {
@@ -13,6 +29,14 @@ describe("CreateObject", () => {
             allowNoParentPid: false,
             allowChangeParentPid: true,
         };
+        ajax = {
+            apiUrl: "http://foo",
+            getJobUrl: jest.fn(),
+            getJSON: jest.fn(),
+            getJSONPromise: jest.fn(() => new Promise(jest.fn(), jest.fn())),
+            ajax: jest.fn(),
+        };
+        AjaxHelper.getInstance.mockReturnValue(ajax);
     });
 
     it("renders appropriately with default settings", () => {
@@ -45,6 +69,30 @@ describe("CreateObject", () => {
         props.allowChangeParentPid = false;
         expect(() => shallow(<CreateObject {...props} />)).toThrowError(
             "allowNoParentPid=true requires allowChangeParentPid to be true"
+        );
+    });
+
+    it("submits appropriate data in default case", () => {
+        const wrapper = mount(<CreateObject {...props} />);
+        act(() => {
+            nodeSelectFunction(new Event("event-foo"), "model-foo");
+        });
+        wrapper.find("input[name='title']").simulate("change", { target: { value: "Test Title" } });
+        wrapper.find("input[name='parent']").simulate("change", { target: { value: "foo:1234" } });
+        wrapper.find("form").simulate("submit");
+        expect(ajax.ajax).toHaveBeenCalledWith(
+            expect.objectContaining({
+                data: {
+                    model: "model-foo",
+                    noParent: 0,
+                    parent: "foo:1234",
+                    state: "Inactive",
+                    title: "Test Title",
+                },
+                dataType: "json",
+                method: "post",
+                url: "http://foo/edit/object/new",
+            })
         );
     });
 });
