f82202cdb89374de96ace23d41159ff8290de747
Demian Katz <demian.katz@villanova.edu>
2021-09-30 16:41:48 -0400

    Expand test coverage.

--- client/src/Editor/CreateObject.test.js
+++ client/src/Editor/CreateObject.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { beforeEach, describe, expect, it, jest } from "@jest/globals";
 import { act } from "react-dom/test-utils";
+import { waitFor } from "@testing-library/react";
 import { mount, shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import CreateObject from "./CreateObject";
@@ -9,15 +10,21 @@ import AjaxHelper from "../AjaxHelper";
 jest.mock("../AjaxHelper");
 
 let nodeSelectFunction = null;
+let treeItems = null;
 jest.mock("@material-ui/lab", function () {
     return {
-        TreeView: ({ onNodeSelect }) => {
+        TreeView: ({ onNodeSelect, children }) => {
             nodeSelectFunction = onNodeSelect;
-            return "TreeView";
+            treeItems = children;
+            return children;
         },
         TreeItem: () => "TreeItem",
     };
 });
+
+function setFakeModels(callback) {
+    callback(["model-foo", "model-bar", "model-baz"]);
+}
 
 describe("CreateObject", () => {
     let ajax;
@@ -33,13 +40,13 @@ describe("CreateObject", () => {
             apiUrl: "http://foo",
             getJobUrl: jest.fn(),
             getJSON: jest.fn(),
-            getJSONPromise: jest.fn(() => new Promise(jest.fn(), jest.fn())),
+            getJSONPromise: jest.fn(() => new Promise(setFakeModels, jest.fn())),
             ajax: jest.fn(),
         };
         AjaxHelper.getInstance.mockReturnValue(ajax);
     });
 
-    it("renders appropriately with default settings", () => {
+    it("renders appropriately with default settings", async () => {
         const wrapper = shallow(<CreateObject {...props} />);
         expect(toJson(wrapper)).toMatchSnapshot();
     });
@@ -72,14 +79,20 @@ describe("CreateObject", () => {
         );
     });
 
-    it("submits appropriate data in default case", () => {
-        const wrapper = mount(<CreateObject {...props} />);
+    it("submits appropriate data in default case", async () => {
+        let wrapper;
+        await waitFor(() => {
+            wrapper = mount(<CreateObject {...props} />);
+        });
         act(() => {
             nodeSelectFunction(new Event("event-foo"), "model-foo");
+            // Test that category select has no effect:
+            nodeSelectFunction(new Event("event-foo"), "__categoryWillBeIgnored");
         });
         wrapper.find("input[name='title']").simulate("change", { target: { value: "Test Title" } });
         wrapper.find("input[name='parent']").simulate("change", { target: { value: "foo:1234" } });
         wrapper.find("form").simulate("submit");
+        expect(treeItems.length).toEqual(3); // make sure setFakeModels is working
         expect(ajax.ajax).toHaveBeenCalledWith(
             expect.objectContaining({
                 data: {
@@ -96,14 +109,47 @@ describe("CreateObject", () => {
         );
     });
 
-    it("submits appropriate data with no parent", () => {
+    it("pre-fills parent pid using parentPid property", async () => {
         props.parentPid = "foo:1234";
-        props.allowNoParentPid = true;
-        const wrapper = mount(<CreateObject {...props} />);
+        props.allowChangeParentPid = false;
+        let wrapper;
+        await waitFor(() => {
+            wrapper = mount(<CreateObject {...props} />);
+        });
         act(() => {
             nodeSelectFunction(new Event("event-foo"), "model-foo");
         });
         wrapper.find("input[name='title']").simulate("change", { target: { value: "Test Title" } });
+        wrapper.find("form").simulate("submit");
+        expect(treeItems.length).toEqual(3); // make sure setFakeModels is working
+        expect(ajax.ajax).toHaveBeenCalledWith(
+            expect.objectContaining({
+                data: {
+                    model: "model-foo",
+                    noParent: 0,
+                    parent: "foo:1234",
+                    state: "Inactive",
+                    title: "Test Title",
+                },
+                dataType: "json",
+                method: "post",
+                url: "http://foo/edit/object/new",
+            })
+        );
+    });
+
+    it("submits appropriate data with active state and no parent", async () => {
+        props.parentPid = "foo:1234";
+        props.allowNoParentPid = true;
+        let wrapper;
+        await waitFor(() => {
+            wrapper = mount(<CreateObject {...props} />);
+        });
+        act(() => {
+            nodeSelectFunction(new Event("event-foo"), "model-foo");
+        });
+        wrapper.find("input[name='title']").simulate("change", { target: { value: "Test Title" } });
+        wrapper.find("input[name='state'][value='Active']").simulate("change", { target: { value: "Active" } });
         wrapper.find("input[name='noParent']").simulate("change", { target: { checked: true } });
         wrapper.find("form").simulate("submit");
         expect(ajax.ajax).toHaveBeenCalledWith(
@@ -112,7 +158,37 @@ describe("CreateObject", () => {
                     model: "model-foo",
                     noParent: 1,
                     parent: "",
-                    state: "Inactive",
+                    state: "Active",
+                    title: "Test Title",
+                },
+                dataType: "json",
+                method: "post",
+                url: "http://foo/edit/object/new",
+            })
+        );
+    });
+
+    it("checks no parent when parent pid is cleared", async () => {
+        props.parentPid = "foo:1234";
+        props.allowNoParentPid = true;
+        let wrapper;
+        await waitFor(() => {
+            wrapper = mount(<CreateObject {...props} />);
+        });
+        act(() => {
+            nodeSelectFunction(new Event("event-foo"), "model-foo");
+        });
+        wrapper.find("input[name='title']").simulate("change", { target: { value: "Test Title" } });
+        wrapper.find("input[name='state'][value='Active']").simulate("change", { target: { value: "Active" } });
+        wrapper.find("input[name='parent']").simulate("change", { target: { value: "" } });
+        wrapper.find("form").simulate("submit");
+        expect(ajax.ajax).toHaveBeenCalledWith(
+            expect.objectContaining({
+                data: {
+                    model: "model-foo",
+                    noParent: 1,
+                    parent: "",
+                    state: "Active",
                     title: "Test Title",
                 },
                 dataType: "json",
