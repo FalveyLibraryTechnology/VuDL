a8769055e230201217b9a3f0392b96351822ab6b
Demian Katz <demian.katz@villanova.edu>
2021-10-27 13:07:21 -0400

    Get things almost working

--- client/src/Editor/ChildList.jsx
+++ client/src/Editor/ChildList.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { useFetchContext } from "../context";
+import { apiUrl } from "../routes";
+import { Link } from "react-router-dom";
 
 const ChildList = ({ pid = null }) => {
-    return <div>{pid ?? "root"}</div>;
+    const {
+        action: { fetchJSON },
+    } = useFetchContext();
+    const [children, setChildren] = useState([]);
+
+    useEffect(() => {
+        async function loadData() {
+            let data = [];
+            const url = apiUrl + "/edit/object/children" + (pid === null ? "" : "/" + pid);
+            try {
+                data = await fetchJSON(url);
+            } catch (e) {
+                console.error("Problem fetching tree data from " + url);
+            }
+            setChildren(data);
+        }
+        loadData();
+    }, []);
+    const contents = (children?.docs ?? []).map((child) => {
+        return (
+            <li key={pid + "child" + child.id}>
+                <Link to={"/edit/object/" + child.id}>{child.title}</Link>
+            </li>
+        );
+    });
+    return <ul>{contents}</ul>;
 };
 
 ChildList.propTypes = {
