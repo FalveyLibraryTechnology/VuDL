7211016a69fdbba00f0c164acfa529201803f57a
Geoff Scholl <geoffrey.scholl@villanova.edu>
2021-10-29 08:48:46 -0400

    added try catch

--- api/src/routes/edit.ts
+++ api/src/routes/edit.ts
@@ -12,8 +12,14 @@ router.get("/models", requireToken, function (req, res) {
 });
 
 router.get("/breadcrumbs/:pid", pidSanitizer, requireToken, async function (req, res) {
-    const fedoraData = await HierarchyCollector.getInstance().getHierarchy(req.params.pid, false);
-    res.json(fedoraData.getBreadcrumbTrail());
+    try {
+        const fedoraData = await HierarchyCollector.getInstance().getHierarchy(req.params.pid, false);
+        res.json(fedoraData.getBreadcrumbTrail());
+    } catch (e) {
+        console.error("Error retrieving breadcrumbs: " + e);
+    }
+    
+    
 });
 
 module.exports = router;
