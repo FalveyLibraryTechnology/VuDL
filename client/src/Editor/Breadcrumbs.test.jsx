433177673b543097e1c1a8e8d1726a0140c79ed8
Demian Katz <demian.katz@villanova.edu>
2022-01-06 13:55:44 -0500

    Add baseline test suite.

---
+++ client/src/Editor/Breadcrumbs.test.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { beforeEach, describe, expect, it, jest } from "@jest/globals";
+import { waitFor } from "@testing-library/react";
+import { mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import Breadcrumbs from "./Breadcrumbs";
+import { FetchContextProvider } from "../FetchContext";
+import { BrowserRouter } from "react-router-dom";
+
+describe("Breadcrumb", () => {
+    let props;
+    let lastRequestUrl;
+    let breadcrumbResponse = {
+        pid: "foo:1234",
+        title: "Fake Title",
+        parents: [
+            {
+                pid: "foo:1233",
+                title: "Fake Parent",
+                parents: []
+            }
+        ]
+    };
+
+    beforeEach(() => {
+        props = { pid: "foo:1234" };
+        global.fetch = jest.fn((url) => {
+            lastRequestUrl = url;
+            return {
+                ok: true,
+                status: 200,
+                json: async function () {
+                    return breadcrumbResponse;
+                },
+            };
+        });
+    });
+
+    it("renders using ajax-loaded object data", async () => {
+        const wrapper = mount(
+            <BrowserRouter>
+                <FetchContextProvider>
+                    <Breadcrumbs {...props} />
+                </FetchContextProvider>
+            </BrowserRouter>
+        );
+        await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+        expect(lastRequestUrl).toEqual("http://localhost:9000/api/edit/breadcrumbs/foo:1234");
+        wrapper.update();
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
