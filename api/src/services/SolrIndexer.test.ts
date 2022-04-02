3e07da3a44e3c09bd26178e0d9508f457bf97887
Demian Katz <demian.katz@villanova.edu>
2022-04-02 18:26:41 -0400

    Add hierarchy test.

--- api/src/services/SolrIndexer.test.ts
+++ api/src/services/SolrIndexer.test.ts
@@ -50,6 +50,101 @@ describe("SolrIndexer", () => {
         expect(changeSpy).toHaveBeenCalledWith(pid, "1900-01-01T00:00:00Z");
     });
 
+    it("indexes hierarchies correctly", async () => {
+        const changeSpy = jest.spyOn(indexer, "getChangeTrackerDetails").mockResolvedValue({});
+        const pid = "test:123";
+        const parentPid = "test:122";
+        const grandparentPid = "test:121";
+        const collector = HierarchyCollector.getInstance();
+        const recordDetails = {
+            hasModel: ["vudl-system:CoreModel", "vudl-system:DataModel", "vudl-system:ImageData"],
+            sequence: [parentPid + "#1"],
+        };
+        const record = FedoraData.build(pid, { "dc:title": ["page"] }, recordDetails);
+        const parentRecordDetails = {
+            hasModel: ["vudl-system:CoreModel", "vudl-system:CollectionModel", "vudl-system:ListCollection"],
+            sortOn: ["custom"],
+        };
+        const parentRecord = FedoraData.build(parentPid, { "dc:title": ["page list"] }, parentRecordDetails);
+        record.addParent(parentRecord);
+        const grandparentRecordDetails = {
+            hasModel: ["vudl-system:CoreModel", "vudl-system:CollectionModel", "vudl-system:ResourceCollection"],
+        };
+        const grandparentRecord = FedoraData.build(grandparentPid, { "dc:title": ["test record"] }, grandparentRecordDetails);
+        parentRecord.addParent(grandparentRecord);
+        const getHierarchySpy = jest.spyOn(collector, "getHierarchy").mockResolvedValueOnce(record).mockResolvedValueOnce(parentRecord);
+        const result = await indexer.getFields(pid);
+        expect(result).toEqual({
+            allfields: ["page"],
+            collection: "Digital Library",
+            datastream_str_mv: [],
+            "dc.title_txt_mv": ["page"],
+            dc_title_str: "page",
+            fedora_parent_id_str_mv: [parentPid],
+            has_order_str: "no",
+            has_thumbnail_str: "false",
+            hierarchy_all_parents_str_mv: [parentPid, grandparentPid],
+            hierarchy_browse: ["test record{{{_ID_}}}test:121"],
+            hierarchy_first_parent_id_str: grandparentPid,
+            hierarchy_parent_id: [grandparentPid],
+            hierarchy_parent_title: ["test record"],
+            hierarchy_sequence: ["0000000000"],
+            hierarchy_sequence_sort_str: "0000000000",
+            hierarchy_top_id: [grandparentPid],
+            hierarchy_top_title: ["test record"],
+            hierarchytype: "",
+            id: pid,
+            institution: "My University",
+            modeltype_str_mv: recordDetails.hasModel,
+            record_format: "vudl",
+            "relsext.hasModel_txt_mv": recordDetails.hasModel,
+            "relsext.sequence_txt_mv": ["test:122#1"],
+            sequence_test_122_str: "0000000001",
+            title: "page",
+            title_full: "page",
+            title_short: "page",
+            title_sort: "page",
+        });
+        const parentResult = await indexer.getFields(parentPid)
+        expect(parentResult).toEqual({
+            allfields: ["page list"],
+            collection: "Digital Library",
+            collection_title_sort_str: "page list",
+            datastream_str_mv: [],
+            "dc.title_txt_mv": ["page list"],
+            dc_title_str: "page list",
+            fedora_parent_id_str_mv: [grandparentPid],
+            has_order_str: "yes",
+            has_thumbnail_str: "false",
+            hierarchy_all_parents_str_mv: [grandparentPid],
+            hierarchy_browse: ["test record{{{_ID_}}}test:121"],
+            hierarchy_first_parent_id_str: parentPid,
+            hierarchy_parent_id: [grandparentPid],
+            hierarchy_parent_title: ["test record"],
+            hierarchy_sequence: ["0000000000"],
+            hierarchy_sequence_sort_str: "0000000000",
+            hierarchy_top_id: [grandparentPid],
+            hierarchy_top_title: ["test record"],
+            hierarchytype: "",
+            id: parentPid,
+            institution: "My University",
+            modeltype_str_mv: parentRecordDetails.hasModel,
+            "relsext.hasModel_txt_mv": parentRecordDetails.hasModel,
+            record_format: "vudl",
+            "relsext.sortOn_txt_mv": ["custom"],
+            title: "page list",
+            title_full: "page list",
+            title_short: "page list",
+            title_sort: "page list",
+        });
+        expect(getHierarchySpy).toHaveBeenCalledTimes(2);
+        expect(getHierarchySpy).toHaveBeenNthCalledWith(1, pid);
+        expect(getHierarchySpy).toHaveBeenNthCalledWith(2, parentPid);
+        expect(changeSpy).toHaveBeenCalledTimes(2);
+        expect(changeSpy).toHaveBeenNthCalledWith(1, pid, "1900-01-01T00:00:00Z");
+        expect(changeSpy).toHaveBeenNthCalledWith(2, parentPid, "1900-01-01T00:00:00Z");
+    });
+
     it("processes title data correctly", async () => {
         const changeSpy = jest.spyOn(indexer, "getChangeTrackerDetails").mockResolvedValue({});
         const pid = "test:123";
