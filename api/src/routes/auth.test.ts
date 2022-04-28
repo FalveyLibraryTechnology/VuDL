7f1bf969a0ed8141bbea3bbe8176d9ca9bcd11bc
Demian Katz <demian.katz@villanova.edu>
2022-04-28 14:26:59 -0400

    Add more login form tests.

--- api/src/routes/auth.test.ts
+++ api/src/routes/auth.test.ts
@@ -17,9 +17,35 @@ describe("index", () => {
 
     it("displays a login form by default", async () => {
         const response = await request(app).get("/auth/login").expect(StatusCodes.OK);
+        expect(response.text).not.toContain("Login failed; please try again.");
         expect(response.text).toContain(`<form action="/api/auth/login" method="post">`);
         expect(response.text).toContain("Username");
         expect(response.text).toContain("Password");
+        expect(response.text).toContain("Submit");
+    });
+
+    it("displays a fail message when appropriate", async () => {
+        const response = await request(app).get("/auth/login?fail=true").expect(StatusCodes.OK);
+        expect(response.text).toContain("Login failed; please try again.");
+        expect(response.text).toContain(`<form action="/api/auth/login" method="post">`);
+        expect(response.text).toContain("Username");
+        expect(response.text).toContain("Password");
+        expect(response.text).toContain("Submit");
+    });
+
+    it("can disable the password control", async () => {
+        Config.setInstance(
+            new Config({
+                Authentication: {
+                    require_passwords: "false",
+                },
+            })
+        );
+        const response = await request(app).get("/auth/login").expect(StatusCodes.OK);
+        expect(response.text).not.toContain("Login failed; please try again.");
+        expect(response.text).toContain(`<form action="/api/auth/login" method="post">`);
+        expect(response.text).toContain("Username");
+        expect(response.text).not.toContain("Password");
         expect(response.text).toContain("Submit");
     });
 });
