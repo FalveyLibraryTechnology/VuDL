a67bc1cfff8ff9e332eebab5fe06b084940c7d37
Demian Katz <demian.katz@villanova.edu>
2022-05-02 08:52:36 -0400

    Add loading message and types to ObjectSummary component.

--- client/components/edit/ObjectSummary.tsx
+++ client/components/edit/ObjectSummary.tsx
@@ -8,13 +8,18 @@ interface ObjectSummaryProps {
     pid: string;
 }
 
+interface ObjectDetails {
+    metadata: Record<string, Array<string>>;
+}
+
 const ObjectSummary = ({ pid = "" }: ObjectSummaryProps): React.ReactElement => {
     const {
         action: { fetchJSON },
     } = useFetchContext();
-    const [details, setDetails] = useState([]);
+    const [details, setDetails] = useState<ObjectDetails>({ metadata: {} });
+    const [loading, setLoading] = useState(true);
 
-    function extractMetadata(metadata, field, defaultValue) {
+    function extractMetadata(metadata: Record<string, Array<string>>, field: string, defaultValue: string) {
         const values = typeof metadata[field] === "undefined" ? [] : metadata[field];
         return values.length > 0 ? values[0] : defaultValue;
     }
@@ -29,11 +34,12 @@ const ObjectSummary = ({ pid = "" }: ObjectSummaryProps): React.ReactElement => 
                 console.error("Problem fetching object details from " + url);
             }
             setDetails(data);
+            setLoading(false);
         }
         loadData();
     }, []);
     const metadata = details?.metadata ?? [];
-    const title = extractMetadata(metadata, "dc:title", "Title not available");
+    const title = loading ? "Loading..." : extractMetadata(metadata, "dc:title", "Title not available");
     const description = extractMetadata(metadata, "dc:description", "");
     return (
         <div className={styles.infobox}>
