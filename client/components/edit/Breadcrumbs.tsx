f93f898d821a7b97f574ce8cbc7a2b555522b332
Demian Katz <demian.katz@villanova.edu>
2022-08-10 12:16:53 -0400

    Refactor breadcrumb interfaces/support functions to helper; add parent list to modal.

--- client/components/edit/Breadcrumbs.tsx
+++ client/components/edit/Breadcrumbs.tsx
@@ -1,25 +1,9 @@
 import styles from "./Breadcrumbs.module.css";
 import React, { useEffect, useState } from "react";
+import { TreeData, generateBreadcrumbTrails, processBreadcrumbData } from "../../util/Breadcrumbs";
 import { useFetchContext } from "../../context/FetchContext";
 import { getObjectParentsUrl } from "../../util/routes";
 import Link from "next/link";
-
-interface TreeNode {
-    pid: string;
-    title: string;
-    parents: Array<TreeNode>;
-}
-
-interface TreeData {
-    topNodes: Array<string>;
-    records: Record<string, TreeNode>;
-    childLookups: Record<string, Array<string>>;
-}
-
-interface BreadcrumbTrail {
-    pid: string;
-    path: Array<TreeNode>;
-}
 
 interface BreadcrumbsProps {
     pid: string;
@@ -34,41 +18,6 @@ const Breadcrumbs = ({ pid = "" }: BreadcrumbsProps): React.ReactElement => {
         childLookups: {},
         records: {},
     });
-
-    /**
-     * Analyze the raw breadcrumb data, identifying top-level nodes (i.e. nodes with
-     * no parents) and creating lookup tables for children. This makes it easier to
-     * render breadcrumbs from left to right and to find all relevant trails.
-     */
-    function processBreadcrumbData(data: TreeNode): TreeData {
-        const queue: Array<TreeNode> = [data];
-        const topNodes: Array<string> = [];
-        const childLookups: Record<string, Array<string>> = {};
-        const records: Record<string, TreeNode> = {};
-        while (queue.length > 0) {
-            const current = queue.shift();
-            if (!current) {
-                break;
-            }
-            if (current.parents.length === 0) {
-                topNodes.push(current.pid);
-            }
-            records[current.pid] = { pid: current.pid, title: current.title, parents: [] };
-            current.parents.forEach((parent) => {
-                queue.push(parent);
-                if (typeof childLookups[parent.pid] === "undefined") {
-                    childLookups[parent.pid] = [current.pid];
-                } else {
-                    childLookups[parent.pid].push(current.pid);
-                }
-            });
-        }
-        return {
-            topNodes: Array.from(new Set(topNodes)), // deduplicate
-            records,
-            childLookups,
-        };
-    }
 
     useEffect(() => {
         async function loadData() {
@@ -87,41 +36,6 @@ const Breadcrumbs = ({ pid = "" }: BreadcrumbsProps): React.ReactElement => {
         }
         loadData();
     }, []);
-
-    function generateBreadcrumbTrails(treeData: TreeData, pid: string) {
-        // BFS from top (root id) to target pid
-        const queue: Array<BreadcrumbTrail> = (treeData.topNodes ?? []).map((rootId) => {
-            return {
-                pid: rootId,
-                path: [],
-            };
-        });
-        const result: Array<Array<TreeNode>> = [];
-        while (queue.length > 0) {
-            const current = queue.shift();
-            if (!current) {
-                break;
-            }
-            const record = treeData.records[current.pid] ?? {};
-            const path = current.path;
-            path.push(record);
-            (treeData.childLookups[current.pid] ?? []).forEach((childPid: string) => {
-                // At target
-                if (childPid === pid) {
-                    result.push(path);
-                } else {
-                    // Add to queue for more
-                    queue.push({
-                        pid: childPid,
-                        path: [...path], // clone array to avoid multiple references to the same array
-                    });
-                }
-            });
-        }
-        // Even if no trails were found, at least return a single empty array so that we can
-        // display the "Edit Home" link when we process the data.
-        return result.length == 0 ? [[]] : result;
-    }
 
     const allTrails = generateBreadcrumbTrails(treeData, pid);
     const contents = allTrails.map((trail, trailIndex) => {
--- client/components/edit/ParentsModal.tsx
+++ client/components/edit/ParentsModal.tsx
@@ -1,20 +1,15 @@
 import React, { useEffect, useState } from "react";
-import Checkbox from "@mui/material/Checkbox";
 import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
-import FormControl from "@mui/material/FormControl";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormLabel from "@mui/material/FormLabel";
 import Grid from "@mui/material/Grid";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import { useEditorContext } from "../../context/EditorContext";
-import { getObjectRecursiveChildPidsUrl, getObjectStateUrl } from "../../util/routes";
 import { useFetchContext } from "../../context/FetchContext";
 import ObjectLoader from "./ObjectLoader";
+import { getObjectParentsUrl } from "../../util/routes";
+import { TreeNode } from "../../util/Breadcrumbs";
 
 const ParentsModal = (): React.ReactElement => {
     const {
@@ -24,12 +19,31 @@ const ParentsModal = (): React.ReactElement => {
     const {
         action: { fetchJSON, fetchText },
     } = useFetchContext();
-    const [statusMessage, setStatusMessage] = useState<string>("");
-    const [includeChildren, setIncludeChildren] = useState<boolean>(false);
-    const [selectedValue, setSelectedValue] = useState<string>("Inactive");
-    const [childPidResponse, setChildPidResponse] = useState({ loading: true });
+    const [parentData, setParentData] = useState<TreeNode>({});
     const loaded = Object.prototype.hasOwnProperty.call(objectDetailsStorage, parentsModalActivePid);
     const details = loaded ? objectDetailsStorage[parentsModalActivePid] : {};
+
+    useEffect(() => {
+        if (!isParentsModalOpen || parentsModalActivePid === null) {
+            return;
+        }
+        async function loadData() {
+            let data: TreeNode = {
+                pid: parentsModalActivePid,
+                title: details.title ?? "",
+                parents: [],
+            };
+            const url = getObjectParentsUrl(parentsModalActivePid);
+            try {
+                data = await fetchJSON(url);
+            } catch (e) {
+                console.error("Problem fetching breadcrumb data from " + url);
+            }
+            setParentData(data);
+        }
+        loadData();
+    }, [isParentsModalOpen, parentsModalActivePid]);
+
 
     const showSnackbarMessage = (message: string, severity: string) => {
         setSnackbarState({
@@ -39,7 +53,24 @@ const ParentsModal = (): React.ReactElement => {
         });
     };
 
-    const contents = "Coming soon...";
+    const parents = (parentData.parents ?? []).map((parent) => {
+        let parentChain = "";
+        let nextNode = (parent.parents ?? [])[0] ?? null;
+        while (nextNode) {
+            parentChain = nextNode.title + (parentChain.length ? "/" : "") + parentChain;
+            nextNode = (nextNode.parents ?? [])[0] ?? null;
+        }
+        return <tr key={"parentmodal_" + parentsModalActivePid + "_" + parent.pid}>
+            <td><button onClick={() => showSnackbarMessage("TODO", "info")}>X</button></td>
+            <td>{parent.pid ?? ""}</td>
+            <td>{parent.title ?? "Unknown title"}</td>
+            <td>{parentChain}</td>
+        </tr>;
+    });
+    const contents = <>
+        <h3>Parents</h3>
+        <table border="1"><tbody>{parents.length > 0 ? parents : <tr key={"parentmodal_" + parentsModalActivePid + "_null"}><td>No parents defined.</td></tr>}</tbody></table>
+    </>;
 
     return (
         <Dialog className="parentsModal" open={isParentsModalOpen} onClose={toggleParentsModal} fullWidth={true}>
