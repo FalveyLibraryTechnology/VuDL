48005034f7880b2fe0273246067c64a77ee14449
Demian Katz <demian.katz@villanova.edu>
2022-08-10 15:50:13 -0400

    Add accordion to PidPicker.

--- client/components/edit/PidPicker.tsx
+++ client/components/edit/PidPicker.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import ChildList from "./children/ChildList";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 interface PidPickerProps {
     selected: string;
@@ -14,12 +19,29 @@ const PidPicker = ({ selected, setSelected }: PidPickerProps): React.ReactElemen
         </>
     ) : (
         <>
-            <label>
-                Enter PID: <input type="text" value={textboxPid} onChange={(e) => setTextboxPid(e.target.value)} />
-            </label>
-            <button onClick={() => setSelected(textboxPid)}>Set</button>
-            <label>Choose PID from Tree:</label>
-            <ChildList selectCallback={setSelected} />
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>Enter PID manually</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        <label>
+                            Enter PID: <input type="text" value={textboxPid} onChange={(e) => setTextboxPid(e.target.value)} />
+                        </label>
+                        <button onClick={() => setSelected(textboxPid)}>Set</button>
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>Choose PID from Tree</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        <ChildList selectCallback={setSelected} />
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
         </>
     );
 };
