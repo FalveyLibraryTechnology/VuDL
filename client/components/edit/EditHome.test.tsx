813a9498e480a1cc7116e68ba86232cce9fa7f65
Demian Katz <demian.katz@villanova.edu>
2022-08-11 16:13:28 -0400

    Fix broken test.

--- client/components/edit/EditHome.test.tsx
+++ client/components/edit/EditHome.test.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { describe, expect, it } from "@jest/globals";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import toJson from "enzyme-to-json";
 import EditHome from "./EditHome";
 
+const mockUseEditorContext = jest.fn();
+jest.mock("../../context/EditorContext", () => ({
+    useEditorContext: () => {
+        return mockUseEditorContext();
+    },
+}));
+
+jest.mock("./children/ChildList", () => () => "ChildList");
+jest.mock("./EditorSnackbar", () => () => "EditorSnackbar");
+jest.mock("./ParentsModal", () => () => "ParentsModal");
+jest.mock("./StateModal", () => () => "StateModal");
+
 describe("EditHome", () => {
+    let editorValues;
+    beforeEach(() => {
+        editorValues = {
+            action: {
+                initializeCatalog: jest.fn(),
+            },
+        };
+        mockUseEditorContext.mockReturnValue(editorValues);
+    });
     it("renders", () => {
-        const wrapper = shallow(<EditHome />);
+        const wrapper = mount(<EditHome />);
+        expect(editorValues.action.initializeCatalog).toHaveBeenCalled();
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 });
