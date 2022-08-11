5d9782580adae19ccceeb5f9214d3cd0a1476d98
Demian Katz <demian.katz@villanova.edu>
2022-08-11 14:18:25 -0400

    Add delete functionality.

--- client/components/edit/parents/ParentList.tsx
+++ client/components/edit/parents/ParentList.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from "react";
 import { useEditorContext } from "../../../context/EditorContext";
+import { useFetchContext } from "../../../context/FetchContext";
+import { getParentUrl } from "../../../util/routes";
+import Delete from "@mui/icons-material/Delete";
 
 export interface ParentListProps {
     pid: string;
@@ -8,8 +11,17 @@ export interface ParentListProps {
 const ParentList = ({ pid }: ParentListProps): React.ReactElement => {
     const {
         state: { parentDetailsStorage },
-        action: { loadParentDetailsIntoStorage, setSnackbarState },
+        action: {
+            clearPidFromChildListStorage,
+            loadParentDetailsIntoStorage,
+            removeFromObjectDetailsStorage,
+            removeFromParentDetailsStorage,
+            setSnackbarState,
+        },
     } = useEditorContext();
+    const {
+        action: { fetchText },
+    } = useFetchContext();
     const loaded = Object.prototype.hasOwnProperty.call(parentDetailsStorage, pid);
 
     useEffect(() => {
@@ -26,6 +38,27 @@ const ParentList = ({ pid }: ParentListProps): React.ReactElement => {
         });
     };
 
+    const deleteParent = async (parentPid: string) => {
+        const target = getParentUrl(pid, parentPid);
+        let result: string;
+        try {
+            result = await fetchText(target, { method: "DELETE" });
+        } catch (e) {
+            result = (e as Error).message ?? "Unexpected error";
+        }
+        if (result === "ok") {
+            // Clear and reload the cached object and its parents, since these have now changed!
+            removeFromObjectDetailsStorage(pid);
+            removeFromParentDetailsStorage(pid);
+            // Clear any cached lists belonging to the parent PID, because the
+            // order has potentially changed!
+            clearPidFromChildListStorage(parentPid);
+            showSnackbarMessage(`Successfully removed ${pid} from ${parentPid}`, "info");
+        } else {
+            showSnackbarMessage(result, "error");
+        }
+    };
+
     const parents = (loaded ? parentDetailsStorage[pid].parents ?? [] : []).map((parent) => {
         let parentChain = "";
         let nextNode = (parent.parents ?? [])[0] ?? null;
@@ -36,7 +69,9 @@ const ParentList = ({ pid }: ParentListProps): React.ReactElement => {
         return (
             <tr key={"parentmodal_" + pid + "_" + parent.pid}>
                 <td>
-                    <button onClick={() => showSnackbarMessage("TODO", "info")}>X</button>
+                    <button onClick={() => deleteParent(parent.pid)}>
+                        <Delete titleAccess={`Delete parent ${parent.pid}`} />
+                    </button>
                 </td>
                 <td>{parent.pid ?? ""}</td>
                 <td>{parent.title ?? "Unknown title"}</td>
