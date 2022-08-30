b5f4eaf7c3ca05720e11f1d967be1c506ba67405
Demian Katz <demian.katz@villanova.edu>
2022-08-30 13:14:41 -0400

    Simplify EditParentsButton.

--- client/components/edit/EditParentsButton.tsx
+++ client/components/edit/EditParentsButton.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useEditorContext } from "../../context/EditorContext";
-import ObjectLoader from "./ObjectLoader";
 
 export interface ObjectStatusProps {
     pid: string;
@@ -8,22 +7,14 @@ export interface ObjectStatusProps {
 
 export const EditParentsButton = ({ pid }: ObjectStatusProps): React.ReactElement => {
     const {
-        state: { objectDetailsStorage },
         action: { setParentsModalActivePid, toggleParentsModal },
     } = useEditorContext();
-    const loaded = Object.prototype.hasOwnProperty.call(objectDetailsStorage, pid);
 
     const clickAction = () => {
         setParentsModalActivePid(pid);
         toggleParentsModal();
     };
-    const button = loaded ? <button onClick={clickAction}>Edit Parents</button> : null;
-    return (
-        <>
-            <ObjectLoader pid={pid} />
-            {button}
-        </>
-    );
+    return <button onClick={clickAction}>Edit Parents</button>;
 };
 
 export default EditParentsButton;
