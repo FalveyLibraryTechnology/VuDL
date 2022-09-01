8369602bf43f504c2d28902a82cd4256480c4a2d
Demian Katz <demian.katz@villanova.edu>
2022-09-01 16:24:12 -0400

    Simplify array search code.

--- api/src/routes/edit.ts
+++ api/src/routes/edit.ts
@@ -387,9 +387,7 @@ edit.delete("/object/:pid/parent/:parentPid", requireToken, pidAndParentPidSanit
 
         // Validate the input
         const fedoraData = await FedoraDataCollector.getInstance().getHierarchy(pid);
-        const legalParent = fedoraData.parents.reduce((previous, current) => {
-            return previous || (current.pid === parent ? current : false);
-        }, false);
+        const legalParent = fedoraData.parents.find((current) => (current.pid === parent));
         if (!legalParent) {
             res.status(400).send(`${parent} is not an immediate parent of ${pid}.`);
             return;
