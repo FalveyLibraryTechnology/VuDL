f7dc30457e1f64652ea0d41e0250715fcae8e3b4
Demian Katz <demian.katz@villanova.edu>
2022-09-02 09:25:31 -0400

    Expand test.

--- client/util/Breadcrumbs.test.ts
+++ client/util/Breadcrumbs.test.ts
@@ -1,4 +1,29 @@
-import { processBreadcrumbData } from "./Breadcrumbs";
+import { generateBreadcrumbTrails, processBreadcrumbData } from "./Breadcrumbs";
+
+describe("generateBreadcrumbTrails", () => {
+    it("can handle a child with multiple parents and a shared root", () => {
+        const treeData = {
+            topNodes: [ 'root' ],
+            records: {
+              child: { pid: 'child', title: 'child', parents: [] },
+              folder1: { pid: 'folder1', title: 'folder1', parents: [] },
+              folder2: { pid: 'folder2', title: 'folder2', parents: [] },
+              root: { pid: 'root', title: 'root', parents: [] }
+            },
+            childLookups: {
+              folder1: [ 'child' ],
+              folder2: [ 'child' ],
+              root: [ 'folder1', 'folder2' ]
+            }
+        };
+        expect(generateBreadcrumbTrails(treeData, "child")).toEqual(
+            [
+                [{"parents": [], "pid": "root", "title": "root"}, {"parents": [], "pid": "folder1", "title": "folder1"}],
+                [{"parents": [], "pid": "root", "title": "root"}, {"parents": [], "pid": "folder2", "title": "folder2"}],
+            ]
+        );
+    });
+});
 
 describe("processBreadcrumbData", () => {
     it("can handle a child with multiple parents and a shared root", () => {
