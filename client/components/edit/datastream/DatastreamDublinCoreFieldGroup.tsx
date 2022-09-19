e3669bd49444f42a5c433709f5a929982fc4caac
Demian Katz <demian.katz@villanova.edu>
2022-09-19 16:36:10 -0400

    Use key counter to avoid hashing.

--- client/components/edit/datastream/DatastreamDublinCoreFieldGroup.tsx
+++ client/components/edit/datastream/DatastreamDublinCoreFieldGroup.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import { useEditorContext } from "../../../context/EditorContext";
 import DatastreamDublinCoreEditField from "./DatastreamDublinCoreEditField";
 import Grid from "@mui/material/Grid";
-import crypto from "crypto";
+
+// We need to be careful about how we set keys for elements in our control group; if we reuse
+// keys after changing components, then when we add or remove values in the list, React may
+// inappropriately reuse existing elements instead of correctly redrawing the modified list.
+// By maintaining a counter of the number of times impactful changes are made in the list, we
+// can ensure that keys are always unique, and that elements are redrawn when necessary.
+// This feels like a hack, and a better solution would be welcomed!
+const keyCounter: Record<string, number> = {};
 
 const DatastreamDublinCoreFieldGroup = ({ field }: { field: string }): React.ReactElement => {
     const {
@@ -11,21 +18,22 @@ const DatastreamDublinCoreFieldGroup = ({ field }: { field: string }): React.Rea
     } = useEditorContext();
     const catalogData = dublinCoreFieldCatalog[field];
     const values = currentDublinCore[field].map((value: string, i: number) => {
-        // We can't use an index as a key here, as it will cause elements to be reused
-        // inappropriately. Instead we must use a hash of the content to uniquely identify
-        // each input element.
-        const md5 = crypto.createHash("md5").update(value).digest("hex");
-        const key = `${field}_${md5}_${i}`;
+        if (!Object.prototype.hasOwnProperty.call(keyCounter, field)) {
+            keyCounter[field] = 0;
+        }
+        const key = `${field}_${i}_${keyCounter[field]}`;
         const saveChanges = (value: string) => {
             currentDublinCore[field][i] = value;
             setCurrentDublinCore(currentDublinCore);
         };
         const addAbove = () => {
             currentDublinCore[field].splice(i, 0, "");
+            keyCounter[field]++;
             setCurrentDublinCore(currentDublinCore);
         };
         const deleteRow = () => {
             currentDublinCore[field].splice(i, 1);
+            keyCounter[field]++;
             setCurrentDublinCore(currentDublinCore);
         };
         const locked = catalogData.type === "locked";
