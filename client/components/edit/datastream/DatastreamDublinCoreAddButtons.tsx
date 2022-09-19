81f0f2f5d0c3676663c17e22031b280bfd97e9a4
Demian Katz <demian.katz@villanova.edu>
2022-09-19 15:53:10 -0400

    Implement clone feature.

--- client/components/edit/datastream/DatastreamDublinCoreAddButtons.tsx
+++ client/components/edit/datastream/DatastreamDublinCoreAddButtons.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEditorContext } from "../../../context/EditorContext";
+import PidPicker from "../PidPicker";
 
 const DatastreamDublinCoreAddButtons = (): React.ReactElement => {
     const {
-        state: { currentDublinCore, dublinCoreFieldCatalog },
-        action: { setCurrentDublinCore },
+        state: { currentDublinCore, dublinCoreFieldCatalog, objectDetailsStorage },
+        action: { loadObjectDetailsIntoStorage, setCurrentDublinCore },
     } = useEditorContext();
+    const [clonePid, setClonePid] = useState("");
+    const clonePidLoaded = clonePid.length > 0 && Object.prototype.hasOwnProperty.call(objectDetailsStorage, clonePid);
+    const loadClonePid = async (newClonePid: string) => {
+        if (newClonePid.length == 0) {
+            setClonePid("");
+            return;
+        }
+        if (!clonePidLoaded) {
+            let error = false;
+            const errorCallback = () => {
+                error = true;
+            };
+            await loadObjectDetailsIntoStorage(newClonePid, errorCallback);
+            if (error) {
+                alert(`Cannot load PID: ${newClonePid}`);
+                return;
+            }
+            setClonePid(newClonePid);
+        }
+    };
     const addField = (field: string) => {
         if (!Object.prototype.hasOwnProperty.call(currentDublinCore, field)) {
             currentDublinCore[field] = [];
@@ -25,12 +46,25 @@ const DatastreamDublinCoreAddButtons = (): React.ReactElement => {
             );
         }
     }
+    const doClone = async () => {
+        const details = objectDetailsStorage[clonePid] ?? {};
+        const metadata = details.metadata ?? {};
+        for (const field in metadata) {
+            // Don't clone locked fields!
+            if (dublinCoreFieldCatalog[field].type !== "locked") {
+                currentDublinCore[field] = (currentDublinCore[field] ?? []).concat(metadata[field]);
+            }
+        }
+        setCurrentDublinCore(currentDublinCore);
+        setClonePid("");
+    };
     return (
         <>
-            <h3>Add Field:</h3>
+            <h3>Add Blank Field:</h3>
             {buttons}
-            <h3>Other Tools:</h3>
-            <button onClick={() => alert("TODO")}>Clone Metadata</button>
+            <h3>Clone Metadata:</h3>
+            <PidPicker selected={clonePid} setSelected={loadClonePid} />
+            {clonePidLoaded ? <button onClick={doClone}>Clone</button> : null}
         </>
     );
 };
