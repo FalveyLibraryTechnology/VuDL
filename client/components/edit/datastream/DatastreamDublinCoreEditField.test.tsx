422fe1465be0c3b6675038f57968466ea9efbed0
Demian Katz <demian.katz@villanova.edu>
2022-10-05 16:33:52 -0400

    Finish DublinCoreEditField test.

--- client/components/edit/datastream/DatastreamDublinCoreEditField.test.tsx
+++ client/components/edit/datastream/DatastreamDublinCoreEditField.test.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { describe, afterEach, expect, it, jest } from "@jest/globals";
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
+import { act } from "react-dom/test-utils";
 import toJson from "enzyme-to-json";
 import DatastreamDublinCoreEditField from "./DatastreamDublinCoreEditField";
+import NativeSelect from "@mui/material/NativeSelect";
+import { Editor } from "@tinymce/tinymce-react";
 
 describe("DatastreamDublinCoreEditField", () => {
     let setValue: () => void;
@@ -64,10 +67,35 @@ describe("DatastreamDublinCoreEditField", () => {
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
+    it("changes the dropdown value", () => {
+        fieldType = "dropdown";
+        legalValues = ["foo", "bar", "baz"];
+        const wrapper = mount(getField());
+        const select = wrapper.find(NativeSelect);
+        expect(select.props().value).toEqual("foo");
+        act(() => {
+            select.props().onChange({ target: { value: "baz" } });
+        });
+        expect(setValue).toHaveBeenCalledWith("baz");
+    });
+
     it("renders a dropdown field with an unexpected value", () => {
         fieldType = "dropdown";
         const wrapper = shallow(getField());
 
         expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it("saves the HTML editor output", () => {
+        fieldType = "html";
+        const wrapper = mount(getField());
+        const mockEditor = {
+            getContent: jest.fn(),
+        };
+        mockEditor.getContent.mockReturnValue("baz");
+        act(() => {
+            wrapper.find(Editor).props().onBlur({}, mockEditor);
+        });
+        expect(setValue).toHaveBeenCalledWith("baz");
     });
 });
