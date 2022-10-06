a990c38a5793732027644585c8cee13bc428baec
Demian Katz <demian.katz@villanova.edu>
2022-10-06 13:16:20 -0400

    Finish tests.

--- client/components/edit/datastream/DatastreamDublinCoreFieldGroup.test.tsx
+++ client/components/edit/datastream/DatastreamDublinCoreFieldGroup.test.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { describe, afterEach, expect, it, jest } from "@jest/globals";
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import DatastreamDublinCoreFieldGroup from "./DatastreamDublinCoreFieldGroup";
+import DatastreamDublinCoreEditField from "./DatastreamDublinCoreEditField";
+import Delete from "@mui/icons-material/Delete";
+import AddCircle from "@mui/icons-material/AddCircle";
+import { act } from "react-dom/test-utils";
 
 const mockUseEditorContext = jest.fn();
 jest.mock("../../../context/EditorContext", () => ({
@@ -47,5 +51,38 @@ describe("DatastreamDublinCoreFieldGroup", () => {
         const wrapper = shallow(<DatastreamDublinCoreFieldGroup field="dc:identifier" />);
 
         expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it("inserts values above", () => {
+        const wrapper = mount(<DatastreamDublinCoreFieldGroup field="dc:title" />);
+
+        act(() => {
+            wrapper.find(AddCircle).at(1).parent().props().onClick();
+        });
+        expect(editorValues.action.setCurrentDublinCore).toHaveBeenCalledWith({
+            "dc:identifier": ["foo"],
+            "dc:title": ["bar", "", "baz"],
+        });
+    });
+
+    it("deletes rows", () => {
+        const wrapper = mount(<DatastreamDublinCoreFieldGroup field="dc:title" />);
+
+        act(() => {
+            wrapper.find(Delete).at(0).parent().props().onClick();
+        });
+        expect(editorValues.action.setCurrentDublinCore).toHaveBeenCalledWith({
+            "dc:identifier": ["foo"],
+            "dc:title": ["baz"],
+        });
+    });
+
+    it("saves values appropriately", () => {
+        const wrapper = mount(<DatastreamDublinCoreFieldGroup field="dc:title" />);
+        wrapper.find(DatastreamDublinCoreEditField).at(1).props().setValue("xyzzy");
+        expect(editorValues.action.setCurrentDublinCore).toHaveBeenCalledWith({
+            "dc:identifier": ["foo"],
+            "dc:title": ["bar", "xyzzy"],
+        });
     });
 });
