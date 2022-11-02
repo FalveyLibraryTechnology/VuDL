724c0d6786d26e2b3c1734aece70d45d44bde12c
Demian Katz <demian.katz@villanova.edu>
2022-11-02 15:54:53 -0400

    Make BlurSavingTextField more efficient. (#173)

--- client/components/shared/BlurSavingTextField.test.tsx
+++ client/components/shared/BlurSavingTextField.test.tsx
@@ -12,11 +12,14 @@ jest.mock("@mui/material/TextField", function () {
 describe("BlurSavingTextField", () => {
     let props: BlurSavingTextFieldProps;
     let value: string;
+    let setterWasCalled: boolean;
     beforeEach(() => {
         value = "foo";
+        setterWasCalled = false;
         props = {
             value,
             setValue: (x: string) => {
+                setterWasCalled = true;
                 value = x;
             },
         };
@@ -52,6 +55,20 @@ describe("BlurSavingTextField", () => {
                 .props()
                 .onBlur({ target: { value: "bar" } });
         });
+        expect(setterWasCalled).toEqual(true);
         expect(value).toEqual("bar");
+    });
+
+    it("doesn't call the save callback if nothing has changed", () => {
+        const wrapper = mount(<BlurSavingTextField {...props} />);
+        act(() => {
+            wrapper
+                .children()
+                .at(0)
+                .props()
+                .onBlur({ target: { value: "foo" } });
+        });
+        expect(setterWasCalled).toEqual(false);
+        expect(value).toEqual("foo");
     });
 });
