4b4ae470b050039ddf48f6fe121fd2b3842ea9be
Demian Katz <demian.katz@villanova.edu>
2022-11-09 12:06:59 -0500

    Test progress.

--- client/components/edit/datastream/DatastreamProcessMetadataTask.test.tsx
+++ client/components/edit/datastream/DatastreamProcessMetadataTask.test.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { describe, afterEach, expect, it, jest } from "@jest/globals";
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import DatastreamProcessMetadataTask from "./DatastreamProcessMetadataTask";
 import { ProcessMetadataTask } from "../../../context/ProcessMetadataContext";
+import Delete from "@mui/icons-material/Delete";
+import AddCircle from "@mui/icons-material/AddCircle";
+import { act } from "react-dom/test-utils";
 
 const mockUseEditorContext = jest.fn();
 jest.mock("../../../context/EditorContext", () => ({
@@ -65,5 +68,39 @@ describe("DatastreamProcessMetadataTask", () => {
         );
 
         expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it("deletes tasks", () => {
+        const deleteTask = jest.fn();
+        const wrapper = mount(
+            <DatastreamProcessMetadataTask
+                task={task}
+                deleteTask={deleteTask}
+                addBelow={jest.fn()}
+                setAttributes={jest.fn()}
+            />
+        );
+
+        act(() => {
+            wrapper.find(Delete).parent().props().onClick();
+        });
+        expect(deleteTask).toHaveBeenCalled();
+    });
+
+    it("inserts values below", () => {
+        const addBelow = jest.fn();
+        const wrapper = mount(
+            <DatastreamProcessMetadataTask
+                task={task}
+                deleteTask={jest.fn()}
+                addBelow={addBelow}
+                setAttributes={jest.fn()}
+            />
+        );
+
+        act(() => {
+            wrapper.find(AddCircle).parent().props().onClick();
+        });
+        expect(addBelow).toHaveBeenCalled();
     });
 });
