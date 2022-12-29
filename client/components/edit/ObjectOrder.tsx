4d9e50a0ff9798a8a059ba424b7940c03c5648c2
Demian Katz <demian.katz@villanova.edu>
2022-12-29 11:16:46 -0500

    Progress on saving mechanism.

--- client/components/edit/ObjectOrder.tsx
+++ client/components/edit/ObjectOrder.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEditorContext } from "../../context/EditorContext";
+import { getObjectDirectChildPidsUrl } from "../../util/routes";
+import { useFetchContext } from "../../context/FetchContext";
 import ObjectStatus from "./ObjectStatus";
 import Refresh from "@mui/icons-material/Refresh";
 import EditParentsButton from "./EditParentsButton";
@@ -13,15 +15,42 @@ const ObjectOrder = ({ pid }: ObjectOrderProps): React.ReactElement => {
         state: { objectDetailsStorage },
         action: { clearPidFromChildListStorage },
     } = useEditorContext();
-
+    const {
+        action: { fetchJSON, fetchText },
+    } = useFetchContext();
+    const [statusMessage, setStatusMessage] = useState<string>("");
     const currentSort = objectDetailsStorage[pid].sortOn ?? "title";
-    const changeSort = (sort: string) => {
-        if (confirm("Are you sure you want to change the sort?")) {
-            alert(sort);
+    const childPageSize = 1000;
+    const solrSort = "title_sort ASC,id ASC";
+    const changeSort = async (sort: string) => {
+        if (!confirm("Are you sure you want to change the sort?")) {
+            return;
         }
+        setStatusMessage("Saving...");
+        let offset = 0;
+        let response = null;
+        do {
+            const url = getObjectDirectChildPidsUrl(pid, offset, childPageSize, solrSort);
+            response = await fetchJSON(url);
+            for (var x in response.docs ?? []) {
+                const targetPid = response.docs[x].id;
+                if (sort === "custom") {
+                    const newPosition = (offset + parseInt(x) + 1)
+                    setStatusMessage(`Setting ${targetPid} to position ${newPosition}`);
+                    // TODO
+                } else {
+                    setStatusMessage(`Clearing order for ${pid}`);
+                    // TODO
+                }
+            }
+            offset += childPageSize;
+        } while (offset < response.numFound ?? 0);
+        setStatusMessage("");
     }
     const otherSort = currentSort === "title" ? "custom" : "title";
-    return (
+    return statusMessage.length > 0 ? (
+        <div>{statusMessage}</div>
+    ) : (
         <div>
             Current sort: {currentSort}
             <button onClick={() => { changeSort(otherSort)}}>Change to {otherSort}</button>
