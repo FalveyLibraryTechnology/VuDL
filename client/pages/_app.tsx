7bb076c5b57d84272bf82d6315d3824d06e311bd
Chris Hallberg <challber@villanova.edu>
2023-02-14 14:34:28 -0500

    feat: catch ThemeMenu from global-context branch.

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,26 +1,34 @@
 /* eslint react/prop-types: 0 */
 import React from "react";
+import { GlobalContextProvider } from "../context/GlobalContext";
 import { PaginatorContextProvider } from "../context/PaginatorContext";
 import { FetchContextProvider } from "../context/FetchContext";
 
 import "../styles/normalize.css";
-import "../styles/globals.css";
 import "../styles/application.css";
 import "../styles/client.css";
 import "../styles/justgrid.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+
 import LogoutButton from "../components/LogoutButton";
+import ThemeMenu from "../components/ThemeMenu";
 
 function MyApp({ Component, pageProps }: { Component: React.ReactNode }): React.ReactElement {
     return (
-        <PaginatorContextProvider>
-            <div className="logout">
-                <LogoutButton />
+        <GlobalContextProvider>
+            <div className="nav--right">
+                <ThemeMenu />
+                <div className="logout">
+                    <LogoutButton />
+                </div>
             </div>
-            <FetchContextProvider>
-                <Component {...pageProps} />
-            </FetchContextProvider>
-        </PaginatorContextProvider>
+
+            <PaginatorContextProvider>
+                <FetchContextProvider>
+                    <Component {...pageProps} />
+                </FetchContextProvider>
+            </PaginatorContextProvider>
+        </GlobalContextProvider>
     );
 }
 export default MyApp;
