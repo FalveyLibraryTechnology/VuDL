57a1cab70cdf6e03017f347fad22ce0f59a2dfa9
Demian Katz <demian.katz@villanova.edu>
2023-05-16 15:32:17 -0400

    Add uploadTime tests.

--- api/src/models/JobMetadata.test.ts
+++ api/src/models/JobMetadata.test.ts
@@ -2,6 +2,7 @@ import fs = require("fs");
 import Config from "./Config";
 import Job from "./Job";
 import JobMetadata from "./JobMetadata";
+import PageOrder from "./PageOrder";
 import QueueManager from "../services/QueueManager";
 import VideoOrder from "./VideoOrder";
 
@@ -29,28 +30,81 @@ describe("JobMetadata", () => {
         jest.restoreAllMocks();
     });
 
-    it("provides Dublin Core when it exists", () => {
-        const filename = "test1/dc.xml";
-        const existsSpy = jest.spyOn(fs, "existsSync").mockReturnValue(true);
-        const readSpy = jest.spyOn(fs, "readFileSync").mockReturnValue(Buffer.from("foo"));
-        expect(jobMetadata.dublinCore).toEqual("foo");
-        expect(existsSpy).toHaveBeenCalledWith(filename);
-        expect(readSpy).toHaveBeenCalledWith(filename);
+    describe("dublinCore", () => {
+        it("provides Dublin Core when it exists", () => {
+            const filename = "test1/dc.xml";
+            const existsSpy = jest.spyOn(fs, "existsSync").mockReturnValue(true);
+            const readSpy = jest.spyOn(fs, "readFileSync").mockReturnValue(Buffer.from("foo"));
+            expect(jobMetadata.dublinCore).toEqual("foo");
+            expect(existsSpy).toHaveBeenCalledWith(filename);
+            expect(readSpy).toHaveBeenCalledWith(filename);
+        });
     });
 
-    it("automatically creates a video order as needed", () => {
-        expect(jobMetadata.video.raw()).toEqual([]);
+    describe("video", () => {
+        it("automatically creates a video order as needed", () => {
+            expect(jobMetadata.video.raw()).toEqual([]);
+        });
+
+        it("allows setting of a video list", () => {
+            const video = new VideoOrder([]);
+            jobMetadata.video = video;
+            expect(jobMetadata.video).toEqual(video);
+        });
+
+        it("allows setting of a video list from raw data", () => {
+            const video = [{ filename: "a.avi" }];
+            jobMetadata.setVideoFromRaw(video);
+            expect(jobMetadata.video.raw()).toEqual(video);
+        });
     });
 
-    it("allows setting of a video list", () => {
-        const video = new VideoOrder([]);
-        jobMetadata.video = video;
-        expect(jobMetadata.video).toEqual(video);
-    });
+    describe("uploadTime", () => {
+        it("provides the most recent file update time from a job", () => {
+            jobMetadata.order = PageOrder.fromRaw([
+                { filename: "foo.jpg", label: "1" },
+                { filename: "bar.jpg", label: "2" },
+            ]);
+            const oldDate = new Date("2020-01-01T00:00:00");
+            const newDate = new Date("2022-01-01T00:00:00");
+            const statSpy = jest
+                .spyOn(fs, "statSync")
+                .mockReturnValueOnce({ mtime: oldDate } as fs.Stats)
+                .mockReturnValueOnce({ mtime: newDate } as fs.Stats);
+            expect(jobMetadata.uploadTime).toEqual(newDate.getTime() / 1000);
+            expect(statSpy).toHaveBeenCalledTimes(2);
+            expect(statSpy).toHaveBeenCalledWith("test1/foo.jpg");
+            expect(statSpy).toHaveBeenCalledWith("test1/bar.jpg");
+        });
 
-    it("allows setting of a video list from raw data", () => {
-        const video = [{ filename: "a.avi" }];
-        jobMetadata.setVideoFromRaw(video);
-        expect(jobMetadata.video.raw()).toEqual(video);
+        it("ignores exceptions when detecting the most recent file update time from a job", () => {
+            jobMetadata.order = PageOrder.fromRaw([
+                { filename: "foo.jpg", label: "1" },
+                { filename: "bar.jpg", label: "2" },
+            ]);
+            const newDate = new Date("2022-01-01T00:00:00");
+            const kaboom = new Error("kaboom");
+            const errorSpy = jest.spyOn(console, "error").mockImplementation(jest.fn());
+            const statSpy = jest
+                .spyOn(fs, "statSync")
+                .mockImplementationOnce(() => {
+                    throw kaboom;
+                })
+                .mockReturnValueOnce({ mtime: newDate } as fs.Stats);
+            expect(jobMetadata.uploadTime).toEqual(newDate.getTime() / 1000);
+            expect(statSpy).toHaveBeenCalledTimes(2);
+            expect(statSpy).toHaveBeenCalledWith("test1/foo.jpg");
+            expect(statSpy).toHaveBeenCalledWith("test1/bar.jpg");
+            expect(errorSpy).toHaveBeenCalledWith(kaboom);
+        });
+
+        it("provides the directory update time from a job with no files", () => {
+            jobMetadata.order = PageOrder.fromRaw([]);
+            const newDate = new Date("2022-01-01T00:00:00");
+            const statSpy = jest.spyOn(fs, "statSync").mockReturnValueOnce({ mtime: newDate } as fs.Stats);
+            expect(jobMetadata.uploadTime).toEqual(newDate.getTime() / 1000);
+            expect(statSpy).toHaveBeenCalledTimes(1);
+            expect(statSpy).toHaveBeenCalledWith("test1");
+        });
     });
 });
