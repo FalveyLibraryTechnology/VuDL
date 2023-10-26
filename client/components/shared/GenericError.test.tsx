0cc600d0687a0cdae03e8657f6d714624be912d2
Demian Katz <demian.katz@villanova.edu>
2023-10-26 19:00:25 -0400

    Port GenericError.test.tsx.

--- client/components/shared/GenericError.test.tsx
+++ client/components/shared/GenericError.test.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { describe, expect, it } from "@jest/globals";
-import { shallow } from "enzyme";
-import toJson from "enzyme-to-json";
+import renderer from "react-test-renderer";
 import GenericError from "./GenericError";
 
 describe("GenericError", () => {
     it("renders", () => {
         const message = "Test error";
-        const wrapper = shallow(<GenericError message={message} />);
-        expect(toJson(wrapper)).toMatchSnapshot();
-        expect(wrapper.contains(message)).toBeTruthy();
+        const tree = renderer.create(<GenericError message={message} />).toJSON();
+        expect(tree).toMatchSnapshot();
+        expect(JSON.stringify(tree).includes(message)).toBeTruthy();
     });
 });
