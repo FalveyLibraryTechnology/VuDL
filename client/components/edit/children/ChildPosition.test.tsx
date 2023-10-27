61255e07150cde464d3c430b031c2a891c7c627c
Demian Katz <demian.katz@villanova.edu>
2023-10-27 08:32:10 -0400

    Port ChildPosition.test.tsx.

--- client/components/edit/children/ChildPosition.test.tsx
+++ client/components/edit/children/ChildPosition.test.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { beforeEach, describe, expect, it, jest } from "@jest/globals";
-import { shallow, mount } from "enzyme";
 import { act } from "react-dom/test-utils";
-import toJson from "enzyme-to-json";
+import { fireEvent, render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
 import ChildPosition from "./ChildPosition";
-import TextField from "@mui/material/TextField";
 
 const mockUseEditorContext = jest.fn();
 jest.mock("../../../context/EditorContext", () => ({
@@ -47,24 +46,24 @@ describe("ChildPosition", () => {
     it("renders correctly when no sort or sequence exists", () => {
         editorValues.state.objectDetailsStorage[pid] = { pid };
         editorValues.state.objectDetailsStorage[parentPid] = { pid: parentPid };
-        const wrapper = shallow(<ChildPosition pid={pid} parentPid={parentPid} />);
-        expect(toJson(wrapper)).toMatchSnapshot();
+        const tree = renderer.create(<ChildPosition pid={pid} parentPid={parentPid} />).toJSON();
+        expect(tree).toBeNull();
     });
 
     it("renders correctly when a sequence exists and parent is not custom sorted", () => {
         const sequences = [`${parentPid}#73`];
         editorValues.state.objectDetailsStorage[pid] = { pid, sequences };
         editorValues.state.objectDetailsStorage[parentPid] = { pid: parentPid, sortOn: "title" };
-        const wrapper = shallow(<ChildPosition pid={pid} parentPid={parentPid} />);
-        expect(toJson(wrapper)).toMatchSnapshot();
+        const tree = renderer.create(<ChildPosition pid={pid} parentPid={parentPid} />).toJSON();
+        expect(tree).toBeNull();
     });
 
     it("renders correctly when a sequence exists and parent is custom sorted", () => {
         const sequences = [`${parentPid}#73`];
         editorValues.state.objectDetailsStorage[pid] = { pid, sequences };
         editorValues.state.objectDetailsStorage[parentPid] = { pid: parentPid, sortOn: "custom" };
-        const wrapper = shallow(<ChildPosition pid={pid} parentPid={parentPid} />);
-        expect(toJson(wrapper)).toMatchSnapshot();
+        const tree = renderer.create(<ChildPosition pid={pid} parentPid={parentPid} />).toJSON();
+        expect(tree).toMatchSnapshot();
     });
 
     it("saves changes when the input is changed", async () => {
@@ -72,15 +71,11 @@ describe("ChildPosition", () => {
         editorValues.state.objectDetailsStorage[pid] = { pid, sequences };
         editorValues.state.objectDetailsStorage[parentPid] = { pid: parentPid, sortOn: "custom" };
         fetchContextValues.action.fetchText.mockResolvedValue("ok");
-        const wrapper = mount(<ChildPosition pid={pid} parentPid={parentPid} />);
+        render(<ChildPosition pid={pid} parentPid={parentPid} />);
         await act(async () => {
-            wrapper
-                .find(TextField)
-                .find("input")
-                .at(0)
-                .simulate("blur", { target: { value: "66" } });
+            const textField = screen.getByRole("textbox");
+            fireEvent.blur(textField, { target: { value: '66' } });
         });
-        await Promise.resolve();
         expect(fetchContextValues.action.fetchText).toHaveBeenCalledWith(
             "http://localhost:9000/api/edit/object/foo%3A123/positionInParent/foo%3A122",
             { body: 66, method: "PUT" },
@@ -93,15 +88,9 @@ describe("ChildPosition", () => {
         const sequences = [`${parentPid}#73`];
         editorValues.state.objectDetailsStorage[pid] = { pid, sequences };
         editorValues.state.objectDetailsStorage[parentPid] = { pid: parentPid, sortOn: "custom" };
-        const wrapper = mount(<ChildPosition pid={pid} parentPid={parentPid} />);
-        await act(async () => {
-            wrapper
-                .find(TextField)
-                .find("input")
-                .at(0)
-                .simulate("blur", { target: { value: "73" } });
-        });
-        await Promise.resolve();
+        render(<ChildPosition pid={pid} parentPid={parentPid} />);
+        const textField = screen.getByRole("textbox");
+        fireEvent.blur(textField, { target: { value: '73' } });
         expect(fetchContextValues.action.fetchText).not.toHaveBeenCalled();
         expect(editorValues.action.clearPidFromChildListStorage).not.toHaveBeenCalled();
         expect(editorValues.action.removeFromObjectDetailsStorage).not.toHaveBeenCalled();
