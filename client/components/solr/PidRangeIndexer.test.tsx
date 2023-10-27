76394d968ef3bda697e69da6847d5d5cd09db2f3
Demian Katz <demian.katz@villanova.edu>
2023-10-27 09:22:33 -0400

    Port PidRangeIndexer.test.tsx.

--- client/components/solr/PidRangeIndexer.test.tsx
+++ client/components/solr/PidRangeIndexer.test.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { beforeEach, describe, expect, it, jest } from "@jest/globals";
-import { act } from "react-dom/test-utils";
-import { mount } from "enzyme";
-import toJson from "enzyme-to-json";
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import renderer from "react-test-renderer";
 import PidRangeIndexer from "./PidRangeIndexer";
 
 const mockUseFetchContext = jest.fn();
@@ -14,7 +14,6 @@ jest.mock("../../context/FetchContext", () => ({
 
 describe("PidRangeIndexer", () => {
     let fetchContextValues;
-    let wrapper;
     let setResults;
     beforeEach(() => {
         fetchContextValues = {
@@ -24,41 +23,33 @@ describe("PidRangeIndexer", () => {
         };
         mockUseFetchContext.mockReturnValue(fetchContextValues);
         setResults = jest.fn();
-        wrapper = mount(<PidRangeIndexer setResults={setResults} />);
     });
 
     it("renders", () => {
-        expect(toJson(wrapper)).toMatchSnapshot();
+        const tree = renderer.create(<PidRangeIndexer setResults={setResults} />).toJSON();
+        expect(tree).toMatchSnapshot();
     });
 
     it("submits appropriate JSON", async () => {
         fetchContextValues.action.fetchText.mockResolvedValue("testText");
+        render(<PidRangeIndexer setResults={setResults} />);
 
-        await act(async () => {
-            wrapper.find("#pidRangePrefix").simulate("change", {
-                target: {
-                    value: "foo:",
-                },
-            });
-            await wrapper.update();
-            wrapper.find("#pidRangeFrom").simulate("change", {
-                target: {
-                    value: "1",
-                },
-            });
-            await wrapper.update();
-            expect(wrapper.find("#pidRangeFrom").instance().value).toEqual("1");
-            wrapper.find("#pidRangeTo").simulate("change", {
-                target: {
-                    value: "5",
-                },
-            });
-            await wrapper.update();
-            expect(wrapper.find("#pidRangeTo").instance().value).toEqual("5");
-            wrapper.find("button").simulate("click");
+        fireEvent.change(screen.getByRole("textbox", { name: "Prefix:" }), {
+            target: {
+                value: "foo:",
+            },
         });
-
-        await Promise.resolve();
+        fireEvent.change(screen.getByRole("textbox", { name: "From (number):" }), {
+            target: {
+                value: "1",
+            },
+        });
+        fireEvent.change(screen.getByRole("textbox", { name: "To (number):" }), {
+            target: {
+                value: "5",
+            },
+        });
+        await userEvent.setup().click(screen.getByRole("button"));
 
         expect(fetchContextValues.action.fetchText).toHaveBeenCalledWith(
             expect.stringMatching(/messenger\/queuesolrindex/),
@@ -75,12 +66,9 @@ describe("PidRangeIndexer", () => {
         fetchContextValues.action.fetchText.mockRejectedValue({
             message: "testError",
         });
+        render(<PidRangeIndexer setResults={setResults} />);
 
-        await act(async () => {
-            wrapper.find("button").simulate("click");
-        });
-
-        await Promise.resolve();
+        await userEvent.setup().click(screen.getByRole("button"));
 
         expect(fetchContextValues.action.fetchText).toHaveBeenCalled();
         expect(setResults).toHaveBeenCalledWith("testError");
