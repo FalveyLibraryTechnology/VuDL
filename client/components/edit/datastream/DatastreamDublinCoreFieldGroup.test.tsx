e58ec8daf723428b68fe4e22746e83d8cb7ef534
Demian Katz <demian.katz@villanova.edu>
2023-10-28 09:19:46 -0400

    Port DatastreamDublinCoreFieldGroup.test.tsx.

--- client/components/edit/datastream/DatastreamDublinCoreFieldGroup.test.tsx
+++ client/components/edit/datastream/DatastreamDublinCoreFieldGroup.test.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 import { describe, afterEach, expect, it, jest } from "@jest/globals";
-import { mount, shallow } from "enzyme";
-import toJson from "enzyme-to-json";
+import renderer from "react-test-renderer";
 import DatastreamDublinCoreFieldGroup from "./DatastreamDublinCoreFieldGroup";
-import DatastreamDublinCoreEditField from "./DatastreamDublinCoreEditField";
-import Delete from "@mui/icons-material/Delete";
-import AddCircle from "@mui/icons-material/AddCircle";
-import { act } from "react-dom/test-utils";
 
 const mockUseEditorContext = jest.fn();
 jest.mock("../../../context/EditorContext", () => ({
@@ -21,6 +16,10 @@ jest.mock("../../../context/DublinCoreMetadataContext", () => ({
         return mockUseDublinCoreMetadataContext();
     },
 }));
+
+jest.mock("@mui/material/Grid", () => (props) => props.children);
+jest.mock("@mui/icons-material/AddCircle", () => (props) => props.titleAccess);
+jest.mock("@mui/icons-material/Delete", () => (props) => props.titleAccess);
 
 describe("DatastreamDublinCoreFieldGroup", () => {
     let dcValues;
@@ -58,38 +57,39 @@ describe("DatastreamDublinCoreFieldGroup", () => {
     });
 
     it("renders unlocked fields", () => {
-        const wrapper = shallow(<DatastreamDublinCoreFieldGroup field="dc:title" />);
-
-        expect(toJson(wrapper)).toMatchSnapshot();
+        const tree = renderer.create(<DatastreamDublinCoreFieldGroup field="dc:title" />).toJSON();
+        expect(tree).toMatchSnapshot();
     });
 
     it("renders locked fields", () => {
-        const wrapper = shallow(<DatastreamDublinCoreFieldGroup field="dc:identifier" />);
-
-        expect(toJson(wrapper)).toMatchSnapshot();
+        const tree = renderer.create(<DatastreamDublinCoreFieldGroup field="dc:identifier" />).toJSON();
+        expect(tree).toMatchSnapshot();
     });
 
-    it("inserts values above", () => {
-        const wrapper = mount(<DatastreamDublinCoreFieldGroup field="dc:title" />);
-
-        act(() => {
-            wrapper.find(AddCircle).at(1).parent().props().onClick();
+    it("inserts values below", () => {
+        const tree = renderer.create(<DatastreamDublinCoreFieldGroup field="dc:title" />);
+        const buttons = tree.root.findAllByType("button");
+        renderer.act(() => {
+            expect(buttons[2].children[0].props.titleAccess).toEqual("Add Below");
+            buttons[2].props.onClick();
         });
         expect(dcValues.action.addValueBelow).toHaveBeenCalledWith("dc:title", 1, "");
     });
 
     it("deletes rows", () => {
-        const wrapper = mount(<DatastreamDublinCoreFieldGroup field="dc:title" />);
-
-        act(() => {
-            wrapper.find(Delete).at(0).parent().props().onClick();
+        const tree = renderer.create(<DatastreamDublinCoreFieldGroup field="dc:title" />);
+        const buttons = tree.root.findAllByType("button");
+        renderer.act(() => {
+            expect(buttons[1].children[0].props.titleAccess).toEqual("Delete Row");
+            buttons[1].props.onClick();
         });
         expect(dcValues.action.deleteValue).toHaveBeenCalledWith("dc:title", 0);
     });
 
     it("saves values appropriately", () => {
-        const wrapper = mount(<DatastreamDublinCoreFieldGroup field="dc:title" />);
-        wrapper.find(DatastreamDublinCoreEditField).at(1).props().setValue("xyzzy");
+        const tree = renderer.create(<DatastreamDublinCoreFieldGroup field="dc:title" />);
+        const inputs = tree.root.findAllByType("input");
+        inputs[1].props.onBlur({ target: { value: "xyzzy" } });
         expect(dcValues.action.replaceValue).toHaveBeenCalledWith("dc:title", 1, "xyzzy");
     });
 });
