6bd6628a3a25ce7629545aaac14342ad3e349c36
Demian Katz <demian.katz@villanova.edu>
2023-11-14 09:52:10 -0500

    Add Solr tests.

---
+++ api/src/services/Solr.test.ts
@@ -0,0 +1,40 @@
+import Solr from "./Solr";
+import SolrCache from "./SolrCache";
+import * as fs from "fs";
+
+describe("Solr", () => {
+    let solr;
+    beforeEach(() => {
+        solr = Solr.getInstance();
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    it("deletes records", async () => {
+        const updateSpy = jest.spyOn(solr, "updateSolr").mockImplementation(jest.fn());
+        const cacheSpy = jest.spyOn(SolrCache.getInstance(), "purgeFromCacheIfEnabled").mockImplementation(jest.fn());
+        solr.deleteRecord("foo", "bar");
+        expect(updateSpy).toHaveBeenCalledWith("foo", "{\"delete\":{\"query\":\"id:\\\"bar\\\"\"}}");
+        expect(cacheSpy).toHaveBeenCalledWith("bar");
+    });
+
+    it("indexes records", async () => {
+        const updateSpy = jest.spyOn(solr, "updateSolr").mockImplementation(jest.fn());
+        const cacheSpy = jest.spyOn(SolrCache.getInstance(), "writeToCacheIfEnabled").mockImplementation(jest.fn());
+        solr.indexRecord("foo", { id: "bar", foo: "baz" });
+        const expectedDoc = "{\"add\":{\"doc\":{\"id\":\"bar\",\"foo\":\"baz\"}}}";
+        expect(updateSpy).toHaveBeenCalledWith("foo", expectedDoc);
+        expect(cacheSpy).toHaveBeenCalledWith("bar", expectedDoc);
+    });
+
+    it("reindexes from file", async () => {
+        const fileContents = "blah blah";
+        const fileSpy = jest.spyOn(fs, "readFileSync").mockReturnValue(fileContents);
+        const updateSpy = jest.spyOn(solr, "updateSolr").mockImplementation(jest.fn());
+        solr.reindexFromFile("foo", "/blah.txt");
+        expect(fileSpy).toHaveBeenCalledWith("/blah.txt");
+        expect(updateSpy).toHaveBeenCalledWith("foo", fileContents);
+    });
+});
