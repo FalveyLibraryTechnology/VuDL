4e5624e0b1ff40c456c57b14d4138308564be151
Demian Katz <demian.katz@villanova.edu>
2023-11-16 06:37:40 -0500

    Add ability to index from a non-default Solr cache dir.

--- api/scripts/reindex-from-solr-cache.js
+++ api/scripts/reindex-from-solr-cache.js
@@ -1,8 +1,24 @@
 const QueueManager = require("../dist/services/QueueManager").default; // eslint-disable-line @typescript-eslint/no-var-requires
 const SolrCache = require("../dist/services/SolrCache").default; // eslint-disable-line @typescript-eslint/no-var-requires
-
+const fs = require("fs");
 const queue = QueueManager.getInstance();
-const cache = SolrCache.getInstance();
+
+// We can use the first command line argument to override the cache directory
+let cache;
+const args = process.argv.slice(2);
+if (args.length < 1) {
+    cache = SolrCache.getInstance();
+} else {
+    if (args[0] === '-h' || args[0] === '--help') {
+        console.info("Usage: node reindex-from-solr-cache.js [optional cache override directory]");
+        return;
+    }
+    if (!fs.existsSync(args[0])) {
+        console.error(`${args[0]} does not exist.`);
+        return;
+    }
+    cache = new SolrCache(args[0]);
+}
 const cachedItems = cache.getDocumentsFromCache();
 if (cachedItems === false) {
     console.error("Cache is disabled.");
