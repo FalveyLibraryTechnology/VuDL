736a662ea228443fe9c7aaee6086b3ff4323f9d3
Demian Katz <demian.katz@villanova.edu>
2023-12-17 08:44:50 -0500

    Parameterize operation in index-pid-list.js

--- api/scripts/index-pid-list.js
+++ api/scripts/index-pid-list.js
@@ -2,7 +2,7 @@ const QueueManager = require("../dist/services/QueueManager").default; // eslint
 const fs = require('node:fs');
 const readline = require('node:readline');
 
-async function processLineByLine(filename) {
+async function processLineByLine(filename, operation = "index") {
     const queue = QueueManager.getInstance();
     const fileStream = fs.createReadStream(filename);
 
@@ -13,11 +13,11 @@ async function processLineByLine(filename) {
 
     for await (const pid of rl) {
         if (pid.length > 0) {
-            await queue.performIndexOperation(pid, "index", true);
+            await queue.performIndexOperation(pid, operation, true);
         }
     }
 }
-  
+
 const args = process.argv.slice(2);
 if (args.length < 1) {
     console.error("Please provide a filename for your PID list.");
@@ -27,4 +27,4 @@ if (!fs.existsSync(args[0])) {
     console.error(args[0] + " does not exist!");
     return;
 }
-processLineByLine(args[0]);
+processLineByLine(args[0], args[1] ?? "index");
